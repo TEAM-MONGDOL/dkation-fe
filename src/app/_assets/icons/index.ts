37214a5bb5651a41bbcd259f28b531d12816062e
Ginieee <kaj1226@naver.com>
2024-07-21 02:36:53 +0900

    add : keyboardArrowDown.svg 추가

--- src/app/_assets/icons/index.ts
+++ src/app/_assets/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as ClickedRadioButton } from './clickedRadioButton.svg';
 export { default as UnClickedRadioButton } from './unClickedRadioButton.svg';
+export { default as KeyboardArrowDown } from './keyboardArrowDown.svg';
