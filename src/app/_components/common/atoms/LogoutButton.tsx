624bb00ee1d4310cf96d504c59cf947858bb90f6
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-21 01:52:30 +0900

    refactor: change pixel

--- src/app/_components/common/atoms/LogoutButton.tsx
+++ src/app/_components/common/atoms/LogoutButton.tsx
@@ -1,6 +1,6 @@
 const LogoutButton = () => {
   return (
-    <button className="bg-cus-100 rounded-[50px] font-bold text-1 px-4 py-1.5">
+    <button className="bg-cus-100 rounded-full font-bold text-1 px-4 py-1.5">
       로그아웃
     </button>
   );
