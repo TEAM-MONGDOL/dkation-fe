20dd8185a645118d5e8e1f99a0e7b7aeed5b7765
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-21 01:47:28 +0900

    refactor: remove pixel

--- src/app/_components/common/atoms/LogoutButton.tsx
+++ src/app/_components/common/atoms/LogoutButton.tsx
@@ -1,6 +1,6 @@
 const LogoutButton = () => {
   return (
-    <button className="bg-cus-100 rounded-[50px] font-bold text-1 px-[17px] py-[6px]">
+    <button className="bg-cus-100 rounded-[50px] font-bold text-1 px-4 py-1.5">
       로그아웃
     </button>
   );
--- src/app/_components/common/modules/Header.tsx
+++ src/app/_components/common/modules/Header.tsx
@@ -4,8 +4,8 @@ import logo from '@/_assets/images/logo_imsy.png';
 
 const Header = () => {
   return (
-    <div className="px-[20px] items-center py-[11px] flex justify-between border-2 border-cus-100">
-      <Image className="h-[20px] w-[91px]" src={logo} alt="logo" />
+    <div className="px-5 items-center py-3.5 flex justify-between border-2 border-cus-100">
+      <Image className="h-5 w-24" src={logo} alt="logo" />
       <LogoutButton />
     </div>
   );
