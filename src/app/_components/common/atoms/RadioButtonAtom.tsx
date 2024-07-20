1b4dda278a2b61eb8a4316c7b9877be005309a6b
Ginieee <kaj1226@naver.com>
2024-07-21 02:38:05 +0900

    fix : RadioButtonAtom 파라미터 수정

--- src/app/_components/common/atoms/RadioButtonAtom.tsx
+++ src/app/_components/common/atoms/RadioButtonAtom.tsx
@@ -4,20 +4,9 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { ClickedRadioButton, UnClickedRadioButton } from '@/_assets/icons';
 
-const RadioButtonAtom: React.FC = () => {
-  const [isChecked, setIsChecked] = useState<boolean>(false);
-
-  const handleClick = () => {
-    setIsChecked((prevChecked) => {
-      const newCheckedState = !prevChecked;
-      console.log(newCheckedState ? 'Checked' : 'Unchecked!');
-      return newCheckedState;
-    });
-  };
-
+const RadioButtonAtom = ({ isChecked }: { isChecked: boolean }) => {
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
-    <div onClick={handleClick} style={{ display: 'inline-block' }}>
+    <div style={{ display: 'inline-block' }}>
       <Image
         src={isChecked ? ClickedRadioButton : UnClickedRadioButton}
         alt={isChecked ? 'Checked' : 'Unchecked'}
