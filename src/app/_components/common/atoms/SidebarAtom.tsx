4c12897a18f27ba8b0ef1e38cd602bb52358d834
gahyeon <gus_05@naver.com>
2024-07-21 12:27:35 +0900

    feat: 현재경로와 일치하면 font-bold

--- src/app/_components/common/atoms/SidebarAtom.tsx
+++ src/app/_components/common/atoms/SidebarAtom.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface SidebarAtomProps {
   title: string;
@@ -10,6 +10,8 @@ interface SidebarAtomProps {
 
 const SidebarAtom: React.FC<SidebarAtomProps> = ({ title, to }) => {
   const router = useRouter();
+  const pathname = usePathname();
+  const isActive = pathname === to; // 현재 경로와 일치하는지 확인
 
   const handleClick = () => {
     router.push(to);
@@ -18,8 +20,8 @@ const SidebarAtom: React.FC<SidebarAtomProps> = ({ title, to }) => {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
     <div
-      onClick={handleClick}
-      className="w-full py-3 pl-4 flex-grow text-cus-300 text-3 cursor-pointer"
+      onClick={() => router.push(to)}
+      className={`w-full py-3 pl-4 flex-grow cursor-pointer text-cus-300 text-3 ${isActive ? 'font-bold' : ''}`}
     >
       {title}
     </div>
