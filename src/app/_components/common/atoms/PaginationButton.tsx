01b777bf13e1ccf0e001a61a6d0d1a2565ad0be1
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-21 22:23:04 +0900

    refactor: 색상 custom 사용

--- src/app/_components/common/atoms/PaginationButton.tsx
+++ src/app/_components/common/atoms/PaginationButton.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 const PaginationButton = ({ onPage = false, page, onClick }: ButtonProps) => {
   return (
     <button
-      className={`text-2 border border-stroke-100 w-12 h-12 flex items-center justify-center ${onPage ? 'bg-[#374553] text-white' : 'text-[#374553]'}`}
+      className={`text-2 border border-stroke-100 w-12 h-12 flex items-center justify-center ${onPage ? 'bg-cus-200 text-white' : 'text-cus-200'}`}
       onClick={onClick}
     >
       {page}
