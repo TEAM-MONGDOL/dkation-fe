3e6f892246f61b8890dc7d0e55262925d840821a
gahyeon <gus_05@naver.com>
2024-07-21 12:15:03 +0900

    feat: SidebarModule 구현

---
+++ src/app/_components/common/modules/SidebarModule.tsx
@@ -0,0 +1,25 @@
+// src/app/_components/common/atoms/container/SidebarContainer.tsx
+
+import React from 'react';
+import SidebarAtom from '@/_components/common/atoms/SidebarAtom';
+
+interface SidebarModuleProps {
+  items: { id: string; title: string; url: string }[];
+}
+
+const SidebarModule = ({ items }: SidebarModuleProps) => {
+  return (
+    <div className="flex flex-col w-full bg-white border border-stroke-100 rounded-[5px]">
+      {items.map((item, index) => (
+        <div
+          key={item.id}
+          className={` ${index < items.length - 1 ? 'border-b border-b-stroke-100' : ''}`}
+        >
+          <SidebarAtom title={item.title} to={item.url} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SidebarModule;
