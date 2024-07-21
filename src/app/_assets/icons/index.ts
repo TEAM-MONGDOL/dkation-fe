2e4342cb010f0917f0de3b84212d38b63efdde7b
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-21 19:42:27 +0900

    feat: add nessisary navbar icon

---
+++ src/app/_assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as InfoIcon } from './info.svg';
+export { default as PointIcon } from './point.svg';
+export { default as ProfileIcon } from './profile.svg';
+export { default as WorkationIcon } from './workation.svg';
