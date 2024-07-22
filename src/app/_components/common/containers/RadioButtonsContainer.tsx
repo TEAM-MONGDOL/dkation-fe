c6a6b06f4fc68e0569770bb052c8f1d73e965511
Ginieee <kaj1226@naver.com>
2024-07-22 21:21:08 +0900

    fix: RadioButtonsContainer 수정 (AccordionHeaderModule, AccordionBodyModule 반영)

--- src/app/_components/common/containers/RadioButtonsContainer.tsx
+++ src/app/_components/common/containers/RadioButtonsContainer.tsx
@@ -2,55 +2,39 @@ import { KeyboardArrowDown } from '@/_assets/icons';
 import Image from 'next/image';
 import { useState } from 'react';
 import RadioButtonModule from '../modules/RadioButtonModule';
+import AccordionHeaderModule from '../modules/AccordionHeaderModule';
+import AccordionBodyModule from '../modules/AccordionBodyModule';
 
 const RadioButtonsContainer = ({
   title,
-  currentChecked,
-  setCurrentChecked,
-  optionList,
+  options,
+  selectedOption,
+  setSelectedOption,
 }: {
   title: string;
-  currentChecked: string;
-  setCurrentChecked: (value: string) => void;
-  optionList: string[];
+  options: string[];
+  selectedOption: string;
+  setSelectedOption: (value: string) => void;
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div className="flex w-full flex-col px-3 py-2.5">
-      <div
-        role="presentation"
-        className="cursor-pointer w-full flex items-center justify-between py-2 px-3 gap-x-2"
-        onClick={() => setIsOpen(!isOpen)}
-        onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
-          if (e.key === 'Enter') {
-            setIsOpen(!isOpen);
-          }
-        }}
-      >
-        <div className="font-bold text-3">{title}</div>
-        <Image
-          className={`
-          transform transition-transform duration-300 w-5 h-5 ${isOpen ? 'rotate-180' : 'rotate-0'}
-          `}
-          src={KeyboardArrowDown}
-          alt="Arrow Down"
-        />
-      </div>
-      <div
-        className={`transition-max-height duration-300 ease-in-out overflow-hidden ${isOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
-      >
-        <div className="w-full flex flex-col items-start justify-start gap-y-1.5 px-3 mt-1.5">
-          {optionList.map((option) => (
-            <RadioButtonModule
-              key={`radio-button-${title}-${option}`}
-              option={option}
-              isClicked={currentChecked === option}
-              onClick={() => setCurrentChecked(option)}
-            />
-          ))}
-        </div>
-      </div>
+      <AccordionHeaderModule
+        title={title}
+        isExpanded={isExpanded}
+        setIsExpanded={setIsExpanded}
+      />
+      <AccordionBodyModule isExpanded={isExpanded}>
+        {options.map((option) => (
+          <RadioButtonModule
+            key={`radio-button-${title}-${option}`}
+            option={option}
+            isClicked={selectedOption === option}
+            onClick={() => setSelectedOption(option)}
+          />
+        ))}
+      </AccordionBodyModule>
     </div>
   );
 };
