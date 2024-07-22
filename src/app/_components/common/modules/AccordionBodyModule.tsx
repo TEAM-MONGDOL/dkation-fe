ca43da622a98dcf64c7b9a6402ba15ca58b86a96
Ginieee <kaj1226@naver.com>
2024-07-22 21:20:12 +0900

    feat: AccordionBodyModule 생성

---
+++ src/app/_components/common/modules/AccordionBodyModule.tsx
@@ -0,0 +1,21 @@
+interface AccordionBodyModuleProps {
+  isExpanded: boolean;
+  children: React.ReactNode;
+}
+
+const AccordionBodyModule = ({
+  isExpanded,
+  children,
+}: AccordionBodyModuleProps) => {
+  return (
+    <div
+      className={`w-full transition-max-height duration-300 ease-in-out overflow-hidden ${isExpanded ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+    >
+      <div className="w-full flex flex-col items-start justify-start gap-y-1.5 px-3 mt-1.5">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AccordionBodyModule;
