e60a1909a4a42705a6c40009236147b56c55adf5
Ginieee <kaj1226@naver.com>
2024-07-22 21:19:38 +0900

    fix: CheckboxAtom 수정

--- src/app/_components/common/atoms/CheckboxAtom.tsx
+++ src/app/_components/common/atoms/CheckboxAtom.tsx
@@ -4,17 +4,13 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { ClickedCheckBox, UnClickedCheckBox } from '@/_assets/icons';
 
-const CheckboxAtom = () => {
-  const [isChecked, setIsChecked] = useState<boolean>(false);
+interface CheckboxAtomProps {
+  isChecked: boolean;
+}
 
-  const handleClick = () => {
-    setIsChecked((prevChecked) => {
-      return !prevChecked;
-    });
-  };
-
+const CheckboxAtom = ({ isChecked }: CheckboxAtomProps) => {
   return (
-    <div onClick={handleClick} className="inline-block" role="presentation">
+    <div className="inline-block" role="presentation">
       <Image
         src={isChecked ? ClickedCheckBox : UnClickedCheckBox}
         alt={isChecked ? 'Checked' : 'Unchecked'}
