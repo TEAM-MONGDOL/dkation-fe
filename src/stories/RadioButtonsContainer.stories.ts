7aacb02085ccdf0407f3e42a38ea67b1c764ca5b
Ginieee <kaj1226@naver.com>
2024-07-22 21:21:32 +0900

    feat: CheckboxModule 관련 스토리북 생성 및 RadioButtonsContainer 스토리북 수정

--- src/stories/RadioButtonsContainer.stories.ts
+++ src/stories/RadioButtonsContainer.stories.ts
@@ -9,11 +9,11 @@ const meta: Meta<typeof RadioButtonsContainer> = {
   },
   args: {
     title: 'Title',
-    currentChecked: 'Option 1',
-    setCurrentChecked: (item: string) => {
+    options: ['Option 1', 'Option 2', 'Option 3'],
+    selectedOption: 'Option 1',
+    setSelectedOption: (item: string) => {
       console.log('Clicked!', item);
     },
-    optionList: ['Option 1', 'Option 2', 'Option 3'],
   },
   tags: ['autodocs'],
 };
@@ -25,36 +25,10 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     title: 'Title',
-    currentChecked: 'Option 1',
-    setCurrentChecked: (item: string) => {
+    options: ['Option 1', 'Option 2', 'Option 3'],
+    selectedOption: 'Option 1',
+    setSelectedOption: (item: string) => {
       console.log('Clicked!', item);
     },
-    optionList: ['Option 1', 'Option 2', 'Option 3'],
-  },
-};
-
-export const WithLongTitle: Story = {
-  args: {
-    title: 'This is a long title',
-    currentChecked: 'Option 1',
-    setCurrentChecked: (item: string) => {
-      console.log('Clicked!', item);
-    },
-    optionList: ['Option 1', 'Option 2', 'Option 3'],
-  },
-};
-
-export const WithLongOptions: Story = {
-  args: {
-    title: 'Title',
-    currentChecked: 'This is a long option 1',
-    setCurrentChecked: (item: string) => {
-      console.log('Clicked!', item);
-    },
-    optionList: [
-      'This is a long option 1',
-      'This is a long option 2',
-      'This is a long option 3',
-    ],
   },
 };
