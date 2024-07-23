2ba219182dd2c40e2084f9c50a345d680dd949da
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-24 00:23:58 +0900

    feat: 검색창 필터링버튼 구분하여 생성

--- src/app/_components/common/atoms/FilteringButton.tsx
+++ src/app/_components/common/atoms/FilteringButton.tsx
@@ -4,7 +4,7 @@ import { FilterIcon } from '@/_assets/icons';
 const FilteringButton = () => {
   return (
     <button className="items-center border border-stroke-100 flex px-2 h-11 bg-cus-100 rounded-xl gap-3">
-      <p className="font-bold text-1 text-sub-300">필터링 및 정렬</p>
+      <p className="font-bold text-5 text-sub-300">필터링 및 정렬</p>
       <Image src={FilterIcon} alt="filtericon" />
     </button>
   );
