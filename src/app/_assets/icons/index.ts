caf72b28851b9a1737b82eea6b3aea02c29c5e58
gahyeon <gus_05@naver.com>
2024-07-24 00:43:13 +0900

    feat: 파일 드래그앤드롭 컴포넌트 구현

--- src/app/_assets/icons/index.ts
+++ src/app/_assets/icons/index.ts
@@ -15,3 +15,4 @@ export { default as FilterIcon } from './filter.svg';
 export { default as RightArrowIcon } from './rightarrow.svg';
 export { default as LeftArrowIcon } from './leftarrow.svg';
 export { default as XCircleIcon } from './xcircle.svg';
+export { default as AddFile } from './addfileButton.svg';
