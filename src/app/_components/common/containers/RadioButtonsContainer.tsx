7c0044846088660382c349441b03d18a9df0cb20
Ginieee <kaj1226@naver.com>
2024-07-24 08:58:29 +0900

    fix: 라디오버튼 컨테이너 AccordionHeader 사용하도록 수정

--- src/app/_components/common/containers/RadioButtonsContainer.tsx
+++ src/app/_components/common/containers/RadioButtonsContainer.tsx
@@ -2,6 +2,7 @@ import { KeyboardArrowDown } from '@/_assets/icons';
 import Image from 'next/image';
 import { useState } from 'react';
 import RadioButtonModule from '../modules/RadioButtonModule';
+import AccordionHeaderModule from '../modules/AccordionHeaderModule';
 
 const RadioButtonsContainer = ({
   title,
@@ -14,31 +15,17 @@ const RadioButtonsContainer = ({
   setCurrentChecked: (value: string) => void;
   optionList: string[];
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <div className="flex w-full flex-col px-3 py-2.5">
+      <AccordionHeaderModule
+        title={title}
+        isExpanded={isExpanded}
+        setIsExpanded={setIsExpanded}
+      />
       <div
-        role="presentation"
-        className="cursor-pointer w-full flex items-center justify-between py-2 px-3 gap-x-2"
-        onClick={() => setIsOpen(!isOpen)}
-        onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
-          if (e.key === 'Enter') {
-            setIsOpen(!isOpen);
-          }
-        }}
-      >
-        <div className="font-bold text-3">{title}</div>
-        <Image
-          className={`
-          transform transition-transform duration-300 w-5 h-5 ${isOpen ? 'rotate-180' : 'rotate-0'}
-          `}
-          src={KeyboardArrowDown}
-          alt="Arrow Down"
-        />
-      </div>
-      <div
-        className={`transition-max-height duration-300 ease-in-out overflow-hidden ${isOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+        className={`transition-max-height duration-300 ease-in-out overflow-hidden ${isExpanded ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
       >
         <div className="w-full flex flex-col items-start justify-start gap-y-1.5 px-3 mt-1.5">
           {optionList.map((option) => (
