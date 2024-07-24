a7eccf09c4f465eb12bd21c08288a29efce05158
gahyeon <gus_05@naver.com>
2024-07-24 13:21:12 +0900

    fix: title을 SelectModule 컴포넌트로 교체

--- src/app/_components/common/containers/SelectContainer.tsx
+++ src/app/_components/common/containers/SelectContainer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import SelectModule from '@/_components/common/modules/SelectModule';
-import CheckboxAtom from '../atoms/CheckboxAtom';
 
 interface SelectContainerProps {
   title: string;
@@ -38,13 +37,11 @@ const SelectContainer = ({
   return (
     <div className="flex flex-col w-[225px] h-[200px]">
       <div className="border border-stroke-100 px-5 py-2.5 rounded-t-regular">
-        <button
-          className="flex items-center justify-start w-full gap-x-10 bg-white text-2 text-sub-300"
+        <SelectModule
+          option={title}
+          isChecked={areAllSelected}
           onClick={handleSelectAllClick}
-        >
-          <CheckboxAtom isChecked={areAllSelected} />
-          <div>{title}</div>
-        </button>
+        />
       </div>
       <div className="overflow-y-auto border border-stroke-100 border-t-0 rounded-b-regular px-5 py-2.5 flex flex-col gap-2">
         {options.map((option) => (
