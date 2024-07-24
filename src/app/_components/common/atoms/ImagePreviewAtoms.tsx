75ae1c51e75e21655d7c07bebb9feb04c40a40a0
gahyeon <gus_05@naver.com>
2024-07-24 13:39:46 +0900

    fix: 이미지 미리보기 사진 비율 유지

--- src/app/_components/common/atoms/ImagePreviewAtoms.tsx
+++ src/app/_components/common/atoms/ImagePreviewAtoms.tsx
@@ -6,13 +6,15 @@ interface ImagePreviewProps {
 
 const ImagePreviewAtom = ({ src }: ImagePreviewProps) => {
   return (
-    <Image
-      src={src}
-      alt="preview"
-      width={80}
-      height={48}
-      className="w-20 h-12 rounded-lg"
-    />
+    <div className="overflow-hidden">
+      <Image
+        src={src}
+        alt="preview"
+        width={80}
+        height={48}
+        className="w-20 h-12 rounded-lg object-cover"
+      />
+    </div>
   );
 };
 
