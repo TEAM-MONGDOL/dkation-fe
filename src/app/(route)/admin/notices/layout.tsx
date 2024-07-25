4bcdce6fc6c82395ad081f6eb52a3d3540dc4238
gahyeon <gus_05@naver.com>
2024-07-25 21:39:10 +0900

    feat: 공지사항 목록 페이지 구현

---
+++ src/app/(route)/admin/notices/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import HeaderModule from '@/_components/common/modules/HeaderModule';
+import NavBarContainer from '@/_components/common/containers/NavBarContainer';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function AdminNoticesLayout({ children }: Props) {
+  return (
+    <div className="flex-col">
+      <HeaderModule />
+      <div className="flex">
+        <NavBarContainer />
+        <main className="flex-1 px-20 py-16">{children}</main>
+      </div>
+    </div>
+  );
+}
+
+export default AdminNoticesLayout;
---
+++ src/app/(route)/admin/notices/page.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { useRouter } from 'next/navigation'; // Import the useRouter hook
+import SearchingBoxModule from '@/_components/common/modules/SearchingBoxModule';
+import PaginationModule from '@/_components/common/modules/PaginationModule';
+import ButtonAtom from '@/_components/common/atoms/ButtonAtom';
+import TitleBarModule from '@/_components/common/modules/TitleBarModule';
+import TableContainer from '@/_components/common/containers/TableContainer';
+
+const headers = [
+  { title: '번호', width: '60' },
+  { title: '구분', width: '100' },
+  { title: '제목', flexGrow: true },
+  { title: '작성일', width: '200' },
+  { title: '상세보기', width: '80' },
+];
+
+const data = [
+  {
+    id: 1,
+    번호: '1',
+    구분: '공지사항',
+    제목: '공지입니다',
+    작성일: '2024-07-20',
+    상세보기: '상세보기',
+  },
+  {
+    id: 2,
+    번호: '2',
+    구분: '이벤트',
+    제목: '이벤트 안내',
+    작성일: '2024-07-21',
+    상세보기: '상세보기',
+  },
+  // 더 많은 데이터
+];
+
+const NoticesListPage = () => {
+  const router = useRouter();
+
+  const moveToWritePage = () => {
+    router.push('/admin/notices/write');
+  };
+
+  return (
+    <div className="w-full">
+      <div className="flex justify-between items-center mb-12">
+        <TitleBarModule title="공지사항 목록" />
+        <div className="flex items-center space-x-4 ml-auto">
+          <SearchingBoxModule placeholder="이름을 검색하세요" filter />
+        </div>
+      </div>
+      <TableContainer headers={headers} data={data} />
+      <div className="flex items-center">
+        <div className="flex-1 flex justify-center">
+          <PaginationModule />
+        </div>
+        <ButtonAtom buttonType="yellow" onClick={moveToWritePage}>
+          글쓰기
+        </ButtonAtom>
+      </div>
+    </div>
+  );
+};
+
+export default NoticesListPage;
