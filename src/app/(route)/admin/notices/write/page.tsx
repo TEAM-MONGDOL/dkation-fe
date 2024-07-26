8b894462d9a6b895e8a6b9339f85ba3c9eb84230
gahyeon <gus_05@naver.com>
2024-07-26 17:02:19 +0900

    feat: 상태 관리를 위한 useState 훅 및 onChange 핸들러 추가

--- src/app/(route)/admin/notices/write/page.tsx
+++ src/app/(route)/admin/notices/write/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import TitleBarModule from '@/_components/common/modules/TitleBarModule';
 import InputModule from '@/_components/common/modules/InputModule';
@@ -11,11 +12,31 @@ import { NoticeOptions } from '@/_constants/common';
 
 const WriteNoticesPage = () => {
   const router = useRouter();
-  const handleSelect = (option: string) => {};
+  const [category, setCategory] = useState('');
+  const [title, setTitle] = useState('');
+  const [files, setFiles] = useState<File[]>([]);
+  const [content, setContent] = useState('');
+
+  const handleSelect = (option: string) => {
+    setCategory(option);
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleFilesChange = (newFiles: File[]) => {
+    setFiles(newFiles);
+  };
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
 
   const handleClick = () => {
     router.push('/admin/notices'); // 추후 수정 예정
   };
+
   return (
     <div>
       <TitleBarModule title="공지 글쓰기" type="LEFT" />
@@ -28,17 +49,25 @@ const WriteNoticesPage = () => {
             placeholder="구분 선택"
           />
           <div className="w-full">
-            <InputModule placeholder="제목을 입력하세요" textCount={20} />
+            <InputModule
+              placeholder="제목을 입력하세요"
+              textCount={20}
+              value={title}
+              onChange={handleTitleChange} // Add onChange handler
+            />
           </div>
         </div>
         <div className="py-7">
-          <FileContainer />
+          <FileContainer onFileChange={handleFilesChange} />{' '}
+          {/* Add onFileChange handler */}
         </div>
         <p className="text-3 font-bold mb-4">내용</p>
         <TextAreaModule
           placeholder="상세 내용을 입력하세요."
           size="LARGE"
           maxLength={2000}
+          value={content}
+          onChange={handleContentChange} // Add onChange handler
         />
         <div className="flex justify-end pt-14">
           <ButtonAtom buttonType="yellow" onClick={handleClick}>
