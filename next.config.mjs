63557a56ae419995ea2135ae9dd1c60f53ad0d61
Ginieee <kaj1226@naver.com>
2024-07-27 03:07:10 +0900

    feat: next.config.mjs에 '/admin/points' > '/admin/points/reward' 리다이렉트 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/admin/points',
+        destination: '/admin/points/reward',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
