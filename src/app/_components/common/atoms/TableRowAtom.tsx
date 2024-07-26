d157a06e5e11fb71e97756911c914675b8c0961b
Ginieee <kaj1226@naver.com>
2024-07-27 03:19:51 +0900

    fix: TableRowAtom에 flex 없애고 td 태그로 수정

--- src/app/_components/common/atoms/TableRowAtom.tsx
+++ src/app/_components/common/atoms/TableRowAtom.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 
 interface TableRowAtomProps {
   content: React.ReactNode;
-  width?: string;
-  flexGrow?: boolean;
   color?: string;
 }
 
-const TableRowAtom = ({
-  content,
-  width,
-  flexGrow,
-  color,
-}: TableRowAtomProps) => {
+const TableRowAtom = ({ content, color }: TableRowAtomProps) => {
   return (
     <td style={{ color }} className="text-center text-3">
       {content}
