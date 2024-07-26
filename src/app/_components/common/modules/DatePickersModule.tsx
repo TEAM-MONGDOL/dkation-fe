4a4343a47e33b1173daa3db26847de3b55328504
Ginieee <kaj1226@naver.com>
2024-07-27 03:13:45 +0900

    fix: DatePickerModuleProps에 nullable 제거, minDate 적용

--- src/app/_components/common/modules/DatePickersModule.tsx
+++ src/app/_components/common/modules/DatePickersModule.tsx
@@ -1,10 +1,11 @@
 import DatePickerAtom from '@/_components/common/atoms/DatePickerAtom';
+import dayjs from 'dayjs';
 
 interface DatePickersModuleProps {
-  startDate: Date | null;
-  setStartDate: (prev: Date | null) => void;
-  endDate: Date | null;
-  setEndDate: (prev: Date | null) => void;
+  startDate: Date;
+  setStartDate: (prev: Date) => void;
+  endDate: Date;
+  setEndDate: (prev: Date) => void;
 }
 
 const DatePickersModule = ({
@@ -15,7 +16,11 @@ const DatePickersModule = ({
 }: DatePickersModuleProps) => {
   return (
     <div className="w-full flex items-center gap-x-1">
-      <DatePickerAtom selectedDate={startDate} setSelectedDate={setStartDate} />
+      <DatePickerAtom
+        selectedDate={startDate}
+        setSelectedDate={setStartDate}
+        minDate={dayjs().add(-1, 'year').toDate()}
+      />
       <span className="font-medium text-sub-100">-</span>
       <DatePickerAtom
         selectedDate={endDate}
