c16d8bfc0f4e7cf07e52481c2fb7917712f5b17e
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-28 18:02:05 +0900

    feat: 워케이션 등록페이지 첫번째 컨테이터 생성

---
+++ src/app/(route)/admin/workation/list/new/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import TitleBarModule from '@/_components/common/modules/TitleBarModule';
+import InputModule from '@/_components/common/modules/InputModule';
+import React, { useState } from 'react';
+import { PlaceGallery } from '@/_assets/icons';
+import Image from 'next/image';
+import DropdownModule from '@/_components/common/modules/DropdownModule';
+import { PlaceOptions } from '@/_constants/common';
+
+const WorkationNew = () => {
+  const [values, setValues] = useState({
+    title: '',
+    category: '',
+  });
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSelect = (option: string) => {
+    setValues({ ...values, category: option });
+  };
+  return (
+    <div>
+      <TitleBarModule title="워케이션 등록" type="LEFT" />
+      <div className="h-52 flex gap-x-8 mt-10">
+        <div className=" gap-y-2 text-sub-200 w-[550px] bg-cus-100 items-center flex flex-col justify-center">
+          <Image src={PlaceGallery} alt="PlaceGallery" />
+          <p className="text-center mt-2">
+            장소 선택 시
+            <br />
+            대표 이미지를 확인할 수 있습니다.
+          </p>
+        </div>
+        <div className="w-full">
+          <div className="flex w-full gap-6">
+            <div className="w-full">
+              <InputModule
+                subtitle="제목"
+                placeholder="제목을 입력하세요"
+                textCount={30}
+                value={values.title}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-52">
+              <InputModule
+                subtitle="모집 인원"
+                placeholder="0"
+                value={values.title}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div className="mt-4 flex flex-col w-full">
+            <p className="text-3 font-semibold">장소</p>
+            <DropdownModule
+              options={PlaceOptions}
+              onSelect={handleSelect}
+              placeholder="장소를 선택해주세요."
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkationNew;
