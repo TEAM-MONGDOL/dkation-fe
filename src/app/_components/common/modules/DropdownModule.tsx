f3c6781a60f9b05b37dbf276e1a98e3b0db3d0bb
gahyeon <gus_05@naver.com>
2024-07-29 22:18:16 +0900

    feat: 배경 클릭 시 드롭다운 메뉴 닫히도록 z-index 설정

--- src/app/_components/common/modules/DropdownModule.tsx
+++ src/app/_components/common/modules/DropdownModule.tsx
@@ -36,33 +36,42 @@ const DropdownModule = ({
   };
 
   return (
-    <div className="relative inline-block text-left w-full">
-      <div>
-        <button
-          type="button"
-          className={`flex items-center justify-between px-4 text-4 text-sub-300 border border-stroke-100 h-5xl ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'rounded-t-regular' : 'rounded-regular'}`}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <span className="text-sub-200">{selectedOption || placeholder}</span>
-          <Image src={DownArrowIcon} alt="downarrow" />
-        </button>
-      </div>
+    <>
       <div
-        className={`absolute left-0 top-5xl w-full rounded-b-regular bg-white border border-stroke-100 border-t-0 transition-transform duration-600 ease-in-out transform ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'translate-y-0 opacity-100 border-t-0' : 'translate-y-[-1px] opacity-0'}`}
-      >
-        {isOpen && (
-          <div>
-            {options.map((option, index) => (
-              <DropdownItemAtom
-                key={option}
-                option={option}
-                onSelect={handleSelect}
-              />
-            ))}
-          </div>
-        )}
+        role="presentation"
+        className={`fixed w-screen top-0 left-0 min-h-screen h-full z-10 ${isOpen ? 'block' : 'hidden'}`}
+        onClick={() => setIsOpen(false)}
+      />
+      <div className="relative inline-block text-left w-full">
+        <div>
+          <button
+            type="button"
+            className={`flex items-center justify-between px-4 text-4 text-sub-300 border border-stroke-100 h-5xl ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'rounded-t-regular' : 'rounded-regular'}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span className="text-sub-200">
+              {selectedOption || placeholder}
+            </span>
+            <Image src={DownArrowIcon} alt="downarrow" />
+          </button>
+        </div>
+        <div
+          className={`absolute left-0 top-5xl w-full rounded-b-regular bg-white border border-stroke-100 border-t-0 transition-transform duration-600 ease-in-out transform z-20 ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'translate-y-0 opacity-100 border-t-0' : 'translate-y-[-1px] opacity-0'}`}
+        >
+          {isOpen && (
+            <div>
+              {options.map((option, index) => (
+                <DropdownItemAtom
+                  key={option}
+                  option={option}
+                  onSelect={handleSelect}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
