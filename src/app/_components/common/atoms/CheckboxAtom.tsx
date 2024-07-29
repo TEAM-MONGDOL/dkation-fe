7ec51220e4c60b3a031ecf1c7c94deb7b54579a6
Ginieee <kaj1226@naver.com>
2024-07-30 00:00:19 +0900

    fix: CheckboxAtom에 onClick과 size 추가

--- src/app/_components/common/atoms/CheckboxAtom.tsx
+++ src/app/_components/common/atoms/CheckboxAtom.tsx
@@ -3,14 +3,18 @@ import { ClickedCheckBox, UnClickedCheckBox } from '@/_assets/icons';
 
 interface CheckboxAtomProps {
   isChecked: boolean;
+  onClick?: () => void;
+  size?: number;
 }
 
-const CheckboxAtom = ({ isChecked }: CheckboxAtomProps) => {
+const CheckboxAtom = ({ isChecked, onClick, size }: CheckboxAtomProps) => {
   return (
-    <div className="inline-block" role="presentation">
+    <div className="inline-block" role="presentation" onClick={onClick}>
       <Image
         src={isChecked ? ClickedCheckBox : UnClickedCheckBox}
         alt={isChecked ? 'Checked' : 'Unchecked'}
+        width={size || 24}
+        height={size || 24}
       />
     </div>
   );
