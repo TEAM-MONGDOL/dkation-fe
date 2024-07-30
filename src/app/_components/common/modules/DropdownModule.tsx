99fdb067d6671597f48568e9c006a575a9c548e1
gahyeon <gus_05@naver.com>
2024-07-30 16:37:50 +0900

    refactor: 드롭다운 컴포넌트 아이콘 파일명 수정

--- src/app/_components/common/modules/DropdownModule.tsx
+++ src/app/_components/common/modules/DropdownModule.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import DropdownItemAtom from '@/_components/common/atoms/DropdownItemAtom';
-import { KeyboardArrowDown } from '@/_assets/icons';
+import { KeyboardArrowDownIcon } from '@/_assets/icons';
 
 interface DropdownModuleProps {
   options: string[];
@@ -39,28 +39,28 @@ const DropdownModule = ({
     <>
       <div
         role="presentation"
-        className={`fixed w-screen top-0 left-0 min-h-screen h-full z-10 ${isOpen ? 'block' : 'hidden'}`}
+        className={`fixed left-0 top-0 z-10 h-full min-h-screen w-screen ${isOpen ? 'block' : 'hidden'}`}
         onClick={() => setIsOpen(false)}
       />
-      <div className="relative inline-block text-left w-full">
+      <div className="relative inline-block w-full text-left">
         <div>
           <button
             type="button"
-            className={`flex items-center justify-between px-4 text-4 text-sub-300 border border-stroke-100 h-5xl ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'rounded-t-regular' : 'rounded-regular'}`}
+            className={`flex h-5xl items-center justify-between border border-stroke-100 px-4 text-4 text-sub-300 ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'rounded-t-regular' : 'rounded-regular'}`}
             onClick={() => setIsOpen(!isOpen)}
           >
             <span className="text-sub-200">
               {selectedOption || placeholder}
             </span>
             <Image
-              src={KeyboardArrowDown}
+              src={KeyboardArrowDownIcon}
               alt="downarrow"
               className={`transform transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
             />
           </button>
         </div>
         <div
-          className={`absolute top-5xl w-full rounded-b-regular bg-white border border-stroke-100 border-t-0 transition-transform duration-600 ease-in-out transform z-20 ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'translate-y-0 opacity-100 border-t-0' : 'translate-y-[-1px] opacity-0'}`}
+          className={`top-5xl duration-600 absolute z-20 w-full transform rounded-b-regular border border-t-0 border-stroke-100 bg-white transition-transform ease-in-out ${fixed ? 'w-52' : 'w-full'} ${isOpen ? 'translate-y-0 border-t-0 opacity-100' : 'translate-y-[-1px] opacity-0'}`}
         >
           {isOpen && (
             <div>
