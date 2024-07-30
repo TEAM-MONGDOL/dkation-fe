00c3d995e044c68a70c90b098c10e78cdd94f466
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-30 17:57:19 +0900

    refactor: 타이틀바모듈 이전페이지 돌아가기 적용

--- src/app/_components/common/modules/TitleBarModule.tsx
+++ src/app/_components/common/modules/TitleBarModule.tsx
@@ -1,15 +1,23 @@
+'use client';
+
 import { KeyboardArrowLeftIcon, KeyboardArrowRightIcon } from '@/_assets/icons';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 interface TypeProps {
   type?: 'LEFT' | 'RIGHT';
   title: string;
 }
 const TitleBarModule = ({ type, title }: TypeProps) => {
+  const router = useRouter();
   return (
     <div className="flex gap-[5px]">
       {type === 'LEFT' && (
-        <Image src={KeyboardArrowLeftIcon} alt="leftarrowicon" />
+        <Image
+          onClick={() => router.back()}
+          src={KeyboardArrowLeftIcon}
+          alt="leftarrowicon"
+        />
       )}
       <p className="text-h2 font-bold">{title}</p>
       {type === 'RIGHT' && (
