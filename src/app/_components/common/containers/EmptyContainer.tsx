e3c5d6549de89ec4c2d5fad8178d1b19256b6595
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-30 11:43:24 +0900

    refactor: heightFull 기본값 설정

--- src/app/_components/common/containers/EmptyContainer.tsx
+++ src/app/_components/common/containers/EmptyContainer.tsx
@@ -3,7 +3,7 @@ import EmptyContentModule from '@/_components/common/modules/EmptyContentModule'
 interface HeightProps {
   heightFull?: boolean;
 }
-const EmptyContainer = ({ heightFull }: HeightProps) => {
+const EmptyContainer = ({ heightFull = false }: HeightProps) => {
   return (
     <div
       className={`w-full ${heightFull ? 'h-full' : ' h-40'} bg-cus-100 content-center`}
