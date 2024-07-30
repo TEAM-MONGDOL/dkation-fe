ff9a6c64bd853a935673b84a774c90ba61949ab3
Ginieee <kaj1226@naver.com>
2024-07-31 01:30:10 +0900

    feat: 헤더 로고 클릭 시 어드민 메인 이동

--- src/app/_components/common/modules/HeaderModule.tsx
+++ src/app/_components/common/modules/HeaderModule.tsx
@@ -1,11 +1,20 @@
+'use client';
+
 import Image from 'next/image';
 import LogoutButtonAtom from '@/_components/common/atoms/LogoutButtonAtom';
 import logo from '@/_assets/images/logo_imsy.png';
+import { useRouter } from 'next/navigation';
 
 const HeaderModule = () => {
+  const router = useRouter();
   return (
     <div className="flex h-6xl items-center justify-between border-b border-stroke-100 px-5 py-3.5">
-      <Image className="h-full max-w-24 object-contain" src={logo} alt="logo" />
+      <Image
+        className="h-full max-w-24 cursor-pointer object-contain"
+        src={logo}
+        alt="logo"
+        onClick={() => router.push('/admin')}
+      />
       <LogoutButtonAtom />
     </div>
   );
