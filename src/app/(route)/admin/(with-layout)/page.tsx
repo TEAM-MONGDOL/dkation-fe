159f1c07e0faa8af02938cf772c5ebf6bd48a3a1
Ginieee <kaj1226@naver.com>
2024-07-31 01:36:31 +0900

    fix: 메인페이지 테이블 헤더에 isFirst, isLast 추가

--- src/app/(route)/admin/(with-layout)/page.tsx
+++ src/app/(route)/admin/(with-layout)/page.tsx
@@ -53,14 +53,16 @@ const AdminMainPage = () => {
         />
         <TableContainer>
           <TableHeaderModule>
-            <TableHeaderAtom width="80px">번호</TableHeaderAtom>
+            <TableHeaderAtom isFirst width="80px">
+              번호
+            </TableHeaderAtom>
             <TableHeaderAtom>워케이션</TableHeaderAtom>
             {/* TODO : API에서 받을 때는 모집 시작일자, 종료일자 따로 받앗어 dayjs를 통한 포맷팅으로 넣어줘야 함 */}
             <TableHeaderAtom width="260px">모집 기간</TableHeaderAtom>
             <TableHeaderAtom width="260px">워케이션 기간</TableHeaderAtom>
             <TableHeaderAtom width="120px">모집 인원</TableHeaderAtom>
             <TableHeaderAtom width="120px">지원자 수</TableHeaderAtom>
-            <TableHeaderAtom width="160px" />
+            <TableHeaderAtom isLast width="160px" />
           </TableHeaderModule>
 
           <tbody>
@@ -96,13 +98,15 @@ const AdminMainPage = () => {
         />
         <TableContainer>
           <TableHeaderModule>
-            <TableHeaderAtom width="80px">번호</TableHeaderAtom>
+            <TableHeaderAtom isFirst width="80px">
+              번호
+            </TableHeaderAtom>
             <TableHeaderAtom width="200px">구분</TableHeaderAtom>
             <TableHeaderAtom width="190px">이름</TableHeaderAtom>
             <TableHeaderAtom>신청 일시</TableHeaderAtom>
             <TableHeaderAtom>심사 일시</TableHeaderAtom>
             <TableHeaderAtom width="160px">상태</TableHeaderAtom>
-            <TableHeaderAtom width="160px" />
+            <TableHeaderAtom isLast width="160px" />
           </TableHeaderModule>
 
           <tbody>
