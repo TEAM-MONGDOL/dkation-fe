285043025ade51c5d7358f4b19a5c10cf6a831b7
은지 <81403265+eunji0714@users.noreply.github.com>
2024-07-30 20:10:15 +0900

    refactor: emptycontainer text prop으로 받기

--- src/app/_components/common/containers/EmptyContainer.tsx
+++ src/app/_components/common/containers/EmptyContainer.tsx
@@ -3,14 +3,15 @@ import EmptyContentModule from '@/_components/common/modules/EmptyContentModule'
 interface HeightProps {
   heightFull?: boolean;
   colSpan?: number;
+  text?: string;
 }
-const EmptyContainer = ({ heightFull = false, colSpan }: HeightProps) => {
+const EmptyContainer = ({ heightFull = false, colSpan, text }: HeightProps) => {
   return (
     <td colSpan={colSpan}>
       <div
         className={`w-full ${heightFull ? 'h-full' : 'h-40'} content-center bg-cus-100`}
       >
-        <EmptyContentModule />
+        <EmptyContentModule text={text} />
       </div>
     </td>
   );
--- src/app/_components/common/modules/EmptyContentModule.tsx
+++ src/app/_components/common/modules/EmptyContentModule.tsx
@@ -2,12 +2,15 @@ import { XCircleIcon } from '@/_assets/icons';
 import { EmptyContainer } from '@/_constants/common';
 import Image from 'next/image';
 
-const EmptyContentModule = () => {
+interface TextProps {
+  text?: string;
+}
+const EmptyContentModule = ({ text }: TextProps) => {
   return (
     <div className="flex flex-col items-center">
       <Image src={XCircleIcon} alt="xcircleicon" />
-      <p className="mt-3 text-sub-200 font-bold text-3">
-        {EmptyContainer.CONTENT}
+      <p className="mt-3 text-3 font-bold text-sub-200">
+        (text ? {text} : {EmptyContainer.CONTENT})
       </p>
     </div>
   );
