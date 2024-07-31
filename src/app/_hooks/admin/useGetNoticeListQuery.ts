4c731667594b360beb0310697be863c2ad1d1ca1
gahyeon <gus_05@naver.com>
2024-07-31 21:04:33 +0900

    feat: useGetNoticeListQuery 구현

---
+++ src/app/_hooks/admin/useGetNoticeListQuery.ts
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+import { announcementListSchema } from '@/_types/adminType';
+import api from '../Axios';
+
+const useGetNoticeListQueryKey = 'useGetNoticeListQuery';
+
+export const useGetNoticeListQuery = ({
+  type,
+  startDate,
+  endDate,
+  pageParam,
+}: {
+  type?: string;
+  startDate?: string;
+  endDate?: string;
+  pageParam: {
+    pageNum: number;
+    pageSize: number;
+    sort?: string;
+  };
+}) => {
+  return useQuery({
+    queryKey: [useGetNoticeListQueryKey],
+    queryFn: async () => {
+      const res = await api.get('api/announcement', {
+        params: {
+          type,
+          startDate,
+          endDate,
+          ...pageParam,
+        },
+      });
+      return announcementListSchema.parse(res.data.data);
+    },
+  });
+};
