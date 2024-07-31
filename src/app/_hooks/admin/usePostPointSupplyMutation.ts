c3c5518b35a37f31bb52f7f29256a20b07cd1c81
Ginieee <kaj1226@naver.com>
2024-07-31 16:52:39 +0900

    feat: usePostPointSupplyMutation 생성

---
+++ src/app/_hooks/admin/usePostPointSupplyMutation.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import api from '../Axios';
+import { useGetPointSupplyQueryKey } from './useGetPointSupplyQuery';
+
+export const usePostPointSupplyMutation = ({
+  successCallback,
+  errorCallback,
+}: {
+  successCallback?: () => void;
+  errorCallback?: (error: Error) => void;
+}) => {
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      policyId,
+      body,
+    }: {
+      policyId: number;
+      body: string[];
+    }) => {
+      await api.post(`/api/point/supply/${policyId}`, body);
+    },
+    onSuccess: () => {
+      client.invalidateQueries({
+        queryKey: [useGetPointSupplyQueryKey],
+      });
+      successCallback && successCallback();
+    },
+    onError: (error: Error) => {
+      errorCallback && errorCallback(error);
+    },
+  });
+};
