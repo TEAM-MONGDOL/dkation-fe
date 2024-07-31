6929fd4ae8a19127942b677d62398e8cd8651021
Ginieee <kaj1226@naver.com>
2024-07-31 09:41:10 +0900

    fix: PointsPolicyNewPage 레이아웃 수정

--- src/app/(route)/admin/(with-layout)/points/policy/new/page.tsx
+++ src/app/(route)/admin/(with-layout)/points/policy/new/page.tsx
@@ -1,78 +1,99 @@
 'use client';
 
-import { InfoIcon } from '@/_assets/icons';
 import PointPolicyInfoModule from '@/_components/admin/points/modules/PointPolicyInfoModule';
 import ButtonAtom from '@/_components/common/atoms/ButtonAtom';
-import InputAreaAtom from '@/_components/common/atoms/InputAreaAtom';
-import InfoSectionContainer from '@/_components/common/containers/InfoSectionContainer';
 import InputModule from '@/_components/common/modules/InputModule';
 import ModalModule from '@/_components/common/modules/ModalModule';
+import TextAreaModule from '@/_components/common/modules/TextAreaModule';
 import TitleBarModule from '@/_components/common/modules/TitleBarModule';
 import dayjs from 'dayjs';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 const AdminPointsPolicyNewPage = () => {
   const router = useRouter();
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [form, setForm] = useState({
+    category: '',
+    point: '',
+    detail: '',
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   return (
-    <div className="w-full flex flex-col gap-y-10 overflow-y-auto">
+    <div className="flex w-full flex-col gap-y-10 overflow-y-auto">
       <TitleBarModule title="포인트 정책 추가" type="LEFT" />
-      <section className="w-full flex flex-col gap-y-[60px]">
-        <div className="w-full flex flex-col gap-y-[30px]">
-          <div className="w-full flex gap-x-[30px] items-center">
+      <section className="flex w-full flex-col gap-y-[60px]">
+        <div className="flex w-full flex-col gap-y-[30px]">
+          <div className="flex w-full items-center gap-x-[30px]">
             <div className="flex-1">
               <InputModule
+                name="category"
                 subtitle="분류"
                 placeholder="분류를 입력해주세요."
                 textCount={20}
+                value={form.category}
+                onChange={handleChange}
               />
             </div>
             <div className="flex-1">
               <InputModule
+                name="point"
                 subtitle="포인트"
                 placeholder="지급할 포인트를 입력해주세요."
-                textCount={20}
+                value={form.point}
+                onChange={handleChange}
+                type="number"
               />
             </div>
           </div>
-          <div className="w-full flex gap-x-[30px] items-center">
+          <div className="flex w-full items-center gap-x-[30px]">
             <div className="flex-1">
-              {/* TODO : ReadOnly 및 background 필요 */}
               <InputModule
                 subtitle="등록일시"
                 value={dayjs().format('YYYY.MM.DD')}
+                status="disabled"
               />
             </div>
             <div className="flex-1" />
           </div>
-          <div className="w-full flex flex-col gap-y-4">
+          <div className="flex w-full flex-col gap-y-4">
             <h3 className="font-bold">상세 내용</h3>
             {/* TODO : 높이조절 필요 */}
-            <InputAreaAtom
+            <TextAreaModule
+              name="detail"
+              size="MEDIUM"
               placeholder="상세 내용을 입력해주세요."
-              textCount={200}
+              maxLength={200}
+              value={form.detail}
+              onChange={handleChange}
             />
           </div>
           <PointPolicyInfoModule />
         </div>
-        <div className="w-full flex items-center justify-end">
+        <div className="flex w-full items-center justify-end">
           <ButtonAtom
-            buttonType="yellow"
+            type="button"
+            buttonStyle="yellow"
             onClick={() => {
               setIsConfirmModalOpen(true);
             }}
-          >
-            등록하기
-          </ButtonAtom>
+            text="추가하기"
+          />
         </div>
         {/* TODO : 머지 후 children에 이미지 넣어야 됨 */}
         {isConfirmModalOpen && (
           <ModalModule
             title="정책을 추가하시겠습니까?"
-            content="정책 추가"
             onConfirm={() => {
               alert('등록하기');
               // TODO : API 연동
@@ -81,7 +102,11 @@ const AdminPointsPolicyNewPage = () => {
             onCancel={() => {
               setIsConfirmModalOpen(false);
             }}
-          />
+          >
+            <p className="text-5 text-sub-200">
+              정책을 추가하시겠습니까? 추가 후에는 수정이 불가능합니다.
+            </p>
+          </ModalModule>
         )}
       </section>
     </div>
