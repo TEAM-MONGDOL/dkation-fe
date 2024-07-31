628511b146cb9ea3851a8ee928500569e008597d
Ginieee <kaj1226@naver.com>
2024-07-31 15:54:52 +0900

    feat: useGetPointSupplyDetailQuery 적용

--- src/app/(route)/admin/(with-layout)/points/reward/[id]/page.tsx
+++ src/app/(route)/admin/(with-layout)/points/reward/[id]/page.tsx
@@ -10,146 +10,111 @@ import SearchingBoxModule from '@/_components/common/modules/SearchingBoxModule'
 import TableBodyModule from '@/_components/common/modules/TableBodyModule';
 import TableHeaderModule from '@/_components/common/modules/TableHeaderModule';
 import TitleBarModule from '@/_components/common/modules/TitleBarModule';
+import { useGetPointSupplyDetailQuery } from '@/_hooks/admin/useGetPointSupplyDetailQuery';
 import { useState } from 'react';
 
-const data = {
-  id: 1,
-  구분: { text: '개인', color: 'red' },
-  분류: '자기계발',
-  이름: '김철수',
-  지급일: '2024-07-20',
-  포인트: 100,
-};
+interface RewardDetailPageProps {
+  params: {
+    id: string;
+  };
+}
 
-const users: {
-  id: number;
-  이름: string;
-  소속: string;
-  아이디: string;
-}[] = [
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-  {
-    id: 1,
-    이름: '김철수',
-    소속: '개발팀',
-    아이디: 'chulsu',
-  },
-];
-
-const AdminPointsRewardDetailPage = () => {
+const AdminPointsRewardDetailPage = ({ params }: RewardDetailPageProps) => {
+  const { id } = params;
+  const { data, isLoading, isError } = useGetPointSupplyDetailQuery({
+    supplyId: Number(id),
+  });
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
     <section className="flex w-full flex-col gap-y-10 overflow-y-auto">
       {/* TODO : Left onClick에 -1 적용 필요 */}
       <TitleBarModule title="포인트 지급 내역 상세" type="LEFT" />
-      <div className="flex w-full items-start gap-x-3xl">
-        <div className="flex min-w-[400px] basis-1/3 flex-col gap-y-3xl">
-          {/* TODO : InputModule에 Readonly 필요, gap 추가 필요 */}
-          <InputModule
-            subtitle="구분"
-            value={data['구분'].text}
-            onChange={() => {}}
-            status="readonly"
-          />
-          <InputModule
-            subtitle="분류"
-            value={data['분류']}
-            onChange={() => {}}
-            status="readonly"
-          />
-          <InputModule
-            subtitle="지급일시"
-            value={data['지급일']}
-            onChange={() => {}}
-            status="readonly"
-          />
-        </div>
-        <div className="flex grow flex-col gap-y-4 rounded-regular border border-stroke-100 bg-cus-100 p-4">
-          <h4 className="font-bold">지급대상 ({users.length})</h4>
-          <div className="flex w-full flex-col gap-y-10">
-            {/* TODO : value, onChange, width 전달해야됨 */}
-            <SearchingBoxModule
-              placeholder="이름을 검색하세요."
-              onClick={() => {}}
-              widthFull
+      {!data ? (
+        isLoading ? (
+          <p>로딩 중...</p>
+        ) : isError ? (
+          <p>에러 발생</p>
+        ) : null
+      ) : (
+        <div className="flex w-full items-start gap-x-3xl">
+          <div className="flex min-w-[400px] basis-1/3 flex-col gap-y-3xl">
+            {/* TODO : InputModule에 Readonly 필요, gap 추가 필요 */}
+            <InputModule
+              subtitle="구분"
+              value={data.pointSupplyInfo.pointSupplyType}
+              onChange={() => {}}
+              status="readonly"
             />
-            <TableContainer>
-              <TableHeaderModule bgColor="bg-cus-100">
-                <TableHeaderAtom isFirst width="80px">
-                  번호
-                </TableHeaderAtom>
-                <TableHeaderAtom>이름</TableHeaderAtom>
-                <TableHeaderAtom>소속</TableHeaderAtom>
-                <TableHeaderAtom isLast>아이디</TableHeaderAtom>
-              </TableHeaderModule>
-              <tbody>
-                {users.length <= 0 ? (
-                  <EmptyContainer colSpan={4} />
-                ) : (
-                  users
-                    .slice(
-                      (currentPage - 1) * 5,
-                      Math.min(users.length, (currentPage - 1) * 5 + 5),
-                    )
-                    .map((user, idx) => (
-                      <TableBodyModule key={user.id}>
-                        <TableBodyAtom isFirst>{idx + 1}</TableBodyAtom>
-                        <TableBodyAtom>{user['이름']}</TableBodyAtom>
-                        <TableBodyAtom>{user['소속']}</TableBodyAtom>
-                        <TableBodyAtom isLast>{user['아이디']}</TableBodyAtom>
-                      </TableBodyModule>
-                    ))
-                )}
-              </tbody>
-            </TableContainer>
+            <InputModule
+              subtitle="분류"
+              value={data.pointSupplyInfo.pointTitle}
+              onChange={() => {}}
+              status="readonly"
+            />
+            <InputModule
+              subtitle="지급일시"
+              value={data.pointSupplyInfo.supplyTime}
+              onChange={() => {}}
+              status="readonly"
+            />
           </div>
-          {users.length > 0 && (
-            <div className="flex w-full items-center justify-center">
-              <PaginationModule
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                totalPages={Math.ceil(users.length / 5)}
+          <div className="flex grow flex-col gap-y-4 rounded-regular border border-stroke-100 bg-cus-100 p-4">
+            <h4 className="font-bold">
+              지급대상 ({data.pointSupplyMemberList.length})
+            </h4>
+            <div className="flex w-full flex-col gap-y-10">
+              {/* TODO : value, onChange, width 전달해야됨 */}
+              <SearchingBoxModule
+                placeholder="이름을 검색하세요."
+                onClick={() => {}}
+                widthFull
               />
+              <TableContainer>
+                <TableHeaderModule bgColor="bg-cus-100">
+                  <TableHeaderAtom isFirst width="80px">
+                    번호
+                  </TableHeaderAtom>
+                  <TableHeaderAtom>이름</TableHeaderAtom>
+                  <TableHeaderAtom>소속</TableHeaderAtom>
+                  <TableHeaderAtom isLast>아이디</TableHeaderAtom>
+                </TableHeaderModule>
+                <tbody>
+                  {data.pointSupplyMemberList.length <= 0 ? (
+                    <EmptyContainer colSpan={4} />
+                  ) : (
+                    data.pointSupplyMemberList
+                      .slice(
+                        (currentPage - 1) * 5,
+                        Math.min(
+                          data.pointSupplyMemberList.length,
+                          (currentPage - 1) * 5 + 5,
+                        ),
+                      )
+                      .map((user, idx) => (
+                        <TableBodyModule key={user.accountId}>
+                          <TableBodyAtom isFirst>{idx + 1}</TableBodyAtom>
+                          <TableBodyAtom>{user.name}</TableBodyAtom>
+                          <TableBodyAtom>{user.department}</TableBodyAtom>
+                          <TableBodyAtom isLast>{user.accountId}</TableBodyAtom>
+                        </TableBodyModule>
+                      ))
+                  )}
+                </tbody>
+              </TableContainer>
             </div>
-          )}
+            {data.pointSupplyMemberList.length > 0 && (
+              <div className="flex w-full items-center justify-center">
+                <PaginationModule
+                  currentPage={currentPage}
+                  setCurrentPage={setCurrentPage}
+                  totalPages={Math.ceil(data.pointSupplyMemberList.length / 5)}
+                />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
