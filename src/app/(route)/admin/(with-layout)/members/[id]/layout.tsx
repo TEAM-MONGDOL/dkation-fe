fbc16b1f5735845e88618b15ba77c9881878d7be
gahyeon <gus_05@naver.com>
2024-08-01 13:13:37 +0900

    refactor: 회원 상세 페이지에서 UserInfoContainer 영역 별도 컴포넌트로 분리

--- src/app/(route)/admin/(with-layout)/members/[id]/layout.tsx
+++ src/app/(route)/admin/(with-layout)/members/[id]/layout.tsx
@@ -2,9 +2,8 @@
 
 import React from 'react';
 import TitleBarModule from '@/_components/common/modules/TitleBarModule';
-import InfoSectionContainer from '@/_components/common/containers/InfoSectionContainer';
 import SidebarModule from '@/_components/common/modules/SidebarModule';
-import { useGetMemberDetailQuery } from '@/_hooks/admin/useGetMemberDetailQuery';
+import UserInfoContainer from '@/_components/admin/member/containers/UserInfoContainer';
 
 interface Props {
   children: React.ReactNode;
@@ -12,63 +11,38 @@ interface Props {
 }
 
 const AdminMembersLayout = ({ children, params }: Props) => {
-  const memberId = params.id;
+  const accountId = params.id;
 
   const membersDetailSidebar = [
     {
       id: '1',
       title: '워케이션 신청 내역',
-      url: `/admin/members/${memberId}/wk-history`,
+      url: `/admin/members/${accountId}/wk-history`,
     },
     {
       id: '2',
       title: '포인트 사용 내역',
-      url: `/admin/members/${memberId}/point-history`,
+      url: `/admin/members/${accountId}/point-history`,
     },
     {
       id: '3',
       title: '페널티 내역',
-      url: `/admin/members/${memberId}/penalty-history`,
+      url: `/admin/members/${accountId}/penalty-history`,
     },
   ];
 
-  const { data, isLoading, isError } = useGetMemberDetailQuery({
-    accountId: memberId,
-  });
-
-  const formattedData = data
-    ? [
-        { subtitle: '이름', content: data.name },
-        { subtitle: '아이디', content: data.accountId },
-        { subtitle: '소속', content: data.department },
-        { subtitle: '보유 포인트', content: `${data.pointQuantity} P` },
-      ]
-    : [];
-
   return (
     <div className="flex h-full flex-col gap-10">
-      {!data ? (
-        isLoading ? (
-          <div>loading ...</div>
-        ) : isError ? (
-          <div>error</div>
-        ) : (
-          <div>no data</div>
-        )
-      ) : (
-        <>
-          <TitleBarModule title="회원 상세" type="LEFT" />
-          <div className="flex gap-x-5">
-            <div className="flex w-[300px] flex-col gap-y-5">
-              <InfoSectionContainer data={formattedData} title="기본 정보" />
-              <SidebarModule items={membersDetailSidebar} />
-            </div>
-            <div className="flex grow">
-              <main className="w-full">{children}</main>
-            </div>
-          </div>
-        </>
-      )}
+      <TitleBarModule title="회원 상세" type="LEFT" />
+      <div className="flex gap-x-5">
+        <div className="flex w-[300px] flex-col gap-y-5">
+          <UserInfoContainer accountId={accountId} />
+          <SidebarModule items={membersDetailSidebar} />
+        </div>
+        <div className="flex grow">
+          <main className="w-full">{children}</main>
+        </div>
+      </div>
     </div>
   );
 };
