81aedfb4442170c8e635f506ae1628c2e1901aa7
Ginieee <kaj1226@naver.com>
2024-08-02 11:25:16 +0900

    feat: useDeletePointPolicyMutation 생성

---
+++ src/app/_hooks/admin/useDeletePointPolicyMutation.ts
@@ -0,0 +1,29 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import api from '../Axios';
+import { useGetPointPolicyQueryKey } from './useGetPointPolicyQuery';
+import { useGetPointPolicyDetailQueryKey } from './useGetPointPolicyDetailQuery';
+
+export const useDeletePointPolicyMutation = ({
+  successCallback,
+  errorCallback,
+}: {
+  successCallback?: () => void;
+  errorCallback?: (error: Error) => void;
+}) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (id: string) => {
+      const res = await api.delete(`/api/point/policy/${id}`);
+      return res;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [useGetPointPolicyQueryKey, useGetPointPolicyDetailQueryKey],
+      });
+      successCallback && successCallback();
+    },
+    onError: (error: Error) => {
+      errorCallback && errorCallback(error);
+    },
+  });
+};
