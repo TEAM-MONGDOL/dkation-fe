c6b7a3f92b1175c8106bc0cefde38e0b4948731e
gahyeon <gus_05@naver.com>
2024-08-03 01:32:29 +0900

    refactor: 공지사항 옵션 옵션명 수정

--- src/app/_constants/common.ts
+++ src/app/_constants/common.ts
@@ -7,7 +7,7 @@ export const DragDropContent = {
   SUBCOMMENT: '파일은 jpeg, png 형식만 첨부할 수 있습니다.',
 } as const;
 
-export const NoticeOptions = ['공지사항', '결과 발표', '이벤트 안내'];
+export const NoticeOptions = ['ANNOUNCEMENT', 'RESULT', 'EVENT'];
 export const PlaceOptions = ['양양', '제주도'];
 export const MembersSearchQueryOptions = {
   NAME: '이름',
