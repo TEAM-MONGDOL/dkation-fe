131eb938aec6f784fadd80f61a81b7fe16ac56c6
Ginieee <kaj1226@naver.com>
2024-08-02 13:24:28 +0900

    feat: usePatchPointPolicyMutation 생성 중

---
+++ src/app/_hooks/admin/usePatchPointPolicyMutation.ts
@@ -0,0 +1,7 @@
+// import { useMutation } from "@tanstack/react-query"
+
+// export const usePatchPointPolicyMutation = (policyId: number;) => {
+//   return useMutation({
+//     mutationFn: async ()
+//   })
+// }
