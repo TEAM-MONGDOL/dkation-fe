c5510518316aee34e3672e7b3c30a98dcd0c485f
Ginieee <kaj1226@naver.com>
2024-08-03 23:35:58 +0900

    feat: usePatchPointPolicyMutation 구현

--- src/app/_hooks/admin/usePatchPointPolicyMutation.ts
+++ src/app/_hooks/admin/usePatchPointPolicyMutation.ts
@@ -1,16 +1,27 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import api from '../Axios';
+import { useGetPointPolicyQueryKey } from './useGetPointPolicyQuery';
+import { useGetPointPolicyDetailQueryKey } from './useGetPointPolicyDetailQuery';
 
-export const usePatchPointPolicyMutation = (policyId: number) => {
+export const usePatchPointPolicyMutation = ({
+  policyId,
+  successCallback,
+  errorCallback,
+}: {
+  policyId: number;
+  successCallback?: () => void;
+  errorCallback?: (error: Error) => void;
+}) => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async ({
       policyTitle,
       detail,
       quantity,
     }: {
-      policyTitle: string;
-      detail: string;
-      quantity: number;
+      policyTitle?: string;
+      detail?: string;
+      quantity?: number;
     }) => {
       const res = await api.patch(`/api/point/policy/${policyId}`, {
         policyTitle,
@@ -18,6 +29,22 @@ export const usePatchPointPolicyMutation = (policyId: number) => {
         quantity,
       });
       return res.data.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [
+          useGetPointPolicyQueryKey,
+          useGetPointPolicyDetailQueryKey,
+          policyId,
+        ],
+      });
+      queryClient.refetchQueries({
+        queryKey: [useGetPointPolicyDetailQueryKey, policyId],
+      });
+      successCallback && successCallback();
+    },
+    onError: (error: Error) => {
+      errorCallback && errorCallback(error);
     },
   });
 };
