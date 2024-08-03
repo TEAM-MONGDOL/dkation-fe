3fcd16e5c208eaf3563ca4ae583b7c29841f79f5
Ginieee <kaj1226@naver.com>
2024-08-03 21:08:09 +0900

    feat: usePatchPointPolicyMutation 생성

--- src/app/_hooks/admin/usePatchPointPolicyMutation.ts
+++ src/app/_hooks/admin/usePatchPointPolicyMutation.ts
@@ -1,7 +1,23 @@
-// import { useMutation } from "@tanstack/react-query"
+import { useMutation } from '@tanstack/react-query';
+import api from '../Axios';
 
-// export const usePatchPointPolicyMutation = (policyId: number;) => {
-//   return useMutation({
-//     mutationFn: async ()
-//   })
-// }
+export const usePatchPointPolicyMutation = (policyId: number) => {
+  return useMutation({
+    mutationFn: async ({
+      policyTitle,
+      detail,
+      quantity,
+    }: {
+      policyTitle: string;
+      detail: string;
+      quantity: number;
+    }) => {
+      const res = await api.patch(`/api/point/policy/${policyId}`, {
+        policyTitle,
+        detail,
+        quantity,
+      });
+      return res.data.data;
+    },
+  });
+};
