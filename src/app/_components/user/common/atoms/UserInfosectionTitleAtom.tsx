738fbfee87f69ad5b2cfd91a3c25efae5f728c3d
gahyeon <gus_05@naver.com>
2024-08-05 22:30:55 +0900

    design: UserInfosectionTitleAtom 너비 161px로 고정

--- src/app/_components/user/common/atoms/UserInfosectionTitleAtom.tsx
+++ src/app/_components/user/common/atoms/UserInfosectionTitleAtom.tsx
@@ -4,7 +4,7 @@ interface UserInfosectionTitleAtomProps {
 
 const UserInfosectionTitleAtom = ({ title }: UserInfosectionTitleAtomProps) => {
   return (
-    <div className="flex h-[72px] items-center bg-sub-100/30 px-12 text-3 text-sub-400">
+    <div className="flex h-[72px] w-[161px] items-center bg-sub-100/30 text-3 text-sub-400">
       {title}
     </div>
   );
