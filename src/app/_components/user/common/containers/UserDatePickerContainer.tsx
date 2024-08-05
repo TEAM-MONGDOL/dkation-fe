78267f5de2813da1ae72e3edcffef12dae9273bb
Ginieee <kaj1226@naver.com>
2024-08-05 21:13:33 +0900

    feat: UserDatePickerContainer에 handleClickTag 적용

--- src/app/_components/user/common/containers/UserDatePickerContainer.tsx
+++ src/app/_components/user/common/containers/UserDatePickerContainer.tsx
@@ -3,6 +3,7 @@ import UserFilteringTitleAtom from '../atoms/UserFilteringTitleAtom';
 import UserDateTagListModule from '../modules/UserDateTagListModule';
 import UserFilteringSubContainer from './UserFilteringSubContainer';
 import UserDatePickersModule from '../modules/UserDatePickerModule';
+import dayjs from 'dayjs';
 
 interface UserDatePickerContainerProps {
   selectedTag: DatePickerTagType;
@@ -21,13 +22,45 @@ const UserDatePickerContainer = ({
   endDate,
   setEndDate,
 }: UserDatePickerContainerProps) => {
+  const handleClickTag = (tag: DatePickerTagType) => {
+    onClickTag(tag);
+    switch (tag) {
+      case 'ALL':
+        setStartDate(null);
+        setEndDate(null);
+        break;
+      case '1_WEEK':
+        setStartDate(dayjs().subtract(1, 'week').toDate());
+        setEndDate(new Date());
+        break;
+      case '1_MONTH':
+        setStartDate(dayjs().subtract(1, 'month').toDate());
+        setEndDate(new Date());
+        break;
+      case '3_MONTH':
+        setStartDate(dayjs().subtract(3, 'month').toDate());
+        setEndDate(new Date());
+        break;
+      case '6_MONTH':
+        setStartDate(dayjs().subtract(6, 'month').toDate());
+        setEndDate(new Date());
+        break;
+      case '1_YEAR':
+        setStartDate(dayjs().subtract(1, 'year').toDate());
+        setEndDate(new Date());
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <UserFilteringSubContainer>
       <UserFilteringTitleAtom text="워케이션 날짜" />
       <div className="flex w-full flex-col gap-y-7">
         <UserDateTagListModule
           selectedTag={selectedTag}
-          onClickTag={onClickTag}
+          onClickTag={(tag: DatePickerTagType) => handleClickTag(tag)}
         />
         <UserDatePickersModule
           startDate={startDate}
