12aa0ae28b37abba1bb1c24d6760fe570d08dd5f
Ginieee <kaj1226@naver.com>
2024-08-05 16:41:30 +0900

    feat: UserDatePickerContainer 생성 (진행 중)

--- src/app/_components/user/common/containers/UserDatePickerContainer.tsx
+++ src/app/_components/user/common/containers/UserDatePickerContainer.tsx
@@ -1,5 +1,28 @@
-const UserDatePickerContainer = () => {
-  return <div></div>;
+import { DatePickerTagType } from '@/_types/commonType';
+import UserFilteringTitleAtom from '../atoms/UserFilteringTitleAtom';
+import UserDateTagListModule from '../modules/UserDateTagListModule';
+import UserFilteringSubContainer from './UserFilteringSubContainer';
+
+interface UserDatePickerContainerProps {
+  selectedTag: DatePickerTagType;
+  onClickTag: (tag: DatePickerTagType) => void;
+}
+
+const UserDatePickerContainer = ({
+  selectedTag,
+  onClickTag,
+}: UserDatePickerContainerProps) => {
+  return (
+    <UserFilteringSubContainer>
+      <UserFilteringTitleAtom text="워케이션 날짜" />
+      <div className="flex w-full flex-col gap-y-7">
+        <UserDateTagListModule
+          selectedTag={selectedTag}
+          onClickTag={onClickTag}
+        />
+      </div>
+    </UserFilteringSubContainer>
+  );
 };
 
 export default UserDatePickerContainer;
