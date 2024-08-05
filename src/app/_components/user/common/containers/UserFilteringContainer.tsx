2c64282b3f38de1d70af4b451b79618fd273bc6f
Ginieee <kaj1226@naver.com>
2024-08-05 15:13:36 +0900

    feat: UserFilteringContainer 파일 생성

---
+++ src/app/_components/user/common/containers/UserFilteringContainer.tsx
@@ -0,0 +1,11 @@
+const UserFilteringContainer = () => {
+  return (
+    <div className="flex w-[380px] flex-col">
+      <div className="flex w-full items-center justify-between px-8 py-9">
+        <span>필터</span>
+      </div>
+    </div>
+  );
+};
+
+export default UserFilteringContainer;
