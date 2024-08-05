13ed2c040f1ac473985e13bf08c5875b22d779fb
은지 <81403265+eunji0714@users.noreply.github.com>
2024-08-05 14:38:51 +0900

    refactor: 워케이션 배너 컨테이너 삭제, 워케이션배너인포모듈에 link 추가

--- src/app/_components/user/common/modules/WkBannerInfoModule.tsx
+++ src/app/_components/user/common/modules/WkBannerInfoModule.tsx
@@ -1,13 +1,18 @@
+import Link from 'next/link';
+
 export interface WkBannerProps {
   percent: number;
   item: string;
   point: number;
+  link: string;
 }
-const WkBannerInfoModule = ({ percent, item, point }: WkBannerProps) => {
+const WkBannerInfoModule = ({ percent, item, point, link }: WkBannerProps) => {
   return (
-    <div className="bg-button flex h-[78px] items-center justify-center text-2 text-white">
-      당신은 지금 상위 {percent}%! {item}에 참여하면 {point}포인트를 드립니다!
-    </div>
+    <Link href={link}>
+      <div className="bg-button flex h-[78px] items-center justify-center text-2 text-white">
+        당신은 지금 상위 {percent}%! {item}에 참여하면 {point}포인트를 드립니다!
+      </div>
+    </Link>
   );
 };
 
