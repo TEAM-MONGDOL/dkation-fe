c37da01ce346ef43cf706773a2af17b70237da08
은지 <81403265+eunji0714@users.noreply.github.com>
2024-08-05 13:27:27 +0900

    refactor: 불필요한 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import HeaderContainers from '@/_components/user/common/containers/HeaderContainers';
 
 const Home = () => {
-  return (
-    <div>
-      <HeaderContainers
-        title="포인트"
-        content="포인트 정책을 확인하여 포인트를 적립하고 사용해보세요."
-      />
-    </div>
-  );
+  return <div>home page</div>;
 };
 
 export default Home;
