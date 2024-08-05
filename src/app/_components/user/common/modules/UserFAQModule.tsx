cf0dce1108661cef5da55b77f64ac9eb8ce81dfd
gahyeon <gus_05@naver.com>
2024-08-05 22:18:03 +0900

    fix: className optional props 삭제

--- src/app/_components/user/common/modules/UserFAQModule.tsx
+++ src/app/_components/user/common/modules/UserFAQModule.tsx
@@ -1,23 +1,16 @@
 'use client';
 
 import { useState } from 'react';
-import Text from '@/_components/user/common/atoms/UserFAQTextAtom';
+import UserFAQText from '@/_components/user/common/atoms/UserFAQTextAtom';
 import { DownArrowIcon } from '@/_assets/icons';
 import Image from 'next/image';
 
 interface UserFAQModuleProps {
   title: string;
   content: string;
-  titleClassName?: string;
-  contentClassName?: string;
 }
 
-const UserFAQModule = ({
-  title,
-  content,
-  titleClassName,
-  contentClassName,
-}: UserFAQModuleProps) => {
+const UserFAQModule = ({ title, content }: UserFAQModuleProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -27,7 +20,7 @@ const UserFAQModule = ({
         className="flex h-[78px] w-full cursor-pointer items-center justify-between border-b border-sub-100 px-3xl"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <Text text={title} className={titleClassName} />
+        <UserFAQText text={title} />
         <Image
           src={DownArrowIcon}
           alt="downArrow"
@@ -38,7 +31,7 @@ const UserFAQModule = ({
         className={`transition-max-height overflow-hidden duration-300 ease-in-out ${isOpen ? 'max-h-96' : 'max-h-0'}`}
       >
         <div className="my-2 bg-sub-100/10 px-3xl py-6 text-sub-300">
-          <Text text={content} className={contentClassName} />
+          <UserFAQText text={content} />
         </div>
       </div>
     </div>
