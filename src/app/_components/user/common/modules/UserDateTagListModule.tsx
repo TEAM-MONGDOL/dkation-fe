78fc08924919add9e7066f0979d2b4728504f3ee
Ginieee <kaj1226@naver.com>
2024-08-05 16:41:14 +0900

    fix: UserDateTagListModule 수정

--- src/app/_components/user/common/modules/UserDateTagListModule.tsx
+++ src/app/_components/user/common/modules/UserDateTagListModule.tsx
@@ -1,26 +1,28 @@
-import { datePickerTagList, DatePickerTagType } from '@/_types/commonType';
+import {
+  datePickerTagConverter,
+  datePickerTagList,
+  DatePickerTagType,
+} from '@/_types/commonType';
 import UserDateTagAtom from '../atoms/UserDateTagAtom';
 
 interface UserDateTagListModuleProps {
-  tags: DatePickerTagType[];
   selectedTag: DatePickerTagType;
   onClickTag: (tag: DatePickerTagType) => void;
 }
 
 const UserDateTagListModule = ({
-  tags,
   selectedTag,
   onClickTag,
 }: UserDateTagListModuleProps) => {
   return (
     <div className="flex w-full flex-wrap gap-2">
-      {tags.map((tag, idx) => {
+      {datePickerTagList.map((tag, idx) => {
         return (
           <UserDateTagAtom
-            text={datePickerTagList[tag]}
+            text={datePickerTagConverter[tag]}
             isClicked={selectedTag === tag}
             onClick={() => onClickTag(tag)}
-            key={'userDatePickerTag' + tag}
+            key={`userDatePickerTag-${tag}`}
           />
         );
       })}
