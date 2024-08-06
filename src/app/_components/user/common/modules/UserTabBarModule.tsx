f9388d81c1acaf713b581275054285503f47377c
Ginieee <kaj1226@naver.com>
2024-08-06 16:43:10 +0900

    fix: UsrTabBarModule의 props > tabs 구조 수정 (text, path)

--- src/app/_components/user/common/modules/UserTabBarModule.tsx
+++ src/app/_components/user/common/modules/UserTabBarModule.tsx
@@ -1,18 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import UserTabBarAtom from '../atoms/UserTabBarAtom';
 
 interface UserTabBarModuleProps {
-  tabs: { text: string; isActive: boolean; onClick: () => void }[];
+  tabs: { text: string; path: string }[];
 }
 
 const UserTabBarModule = ({ tabs }: UserTabBarModuleProps) => {
+  const router = useRouter();
+
   return (
     <div className="flex h-[70px] w-full items-center gap-x-6xl border-b-[0.5px] border-sub-100 px-40 shadow-[0_1.5px_3px_0_rgba(0,0,0,0.2)]">
       {tabs.map((tab, index) => (
         <UserTabBarAtom
           key={tab.text}
           text={tab.text}
-          isActive={tab.isActive}
-          onClick={tab.onClick}
+          isActive={
+            window.location.pathname === tab.path ||
+            (window.location.pathname.includes(tab.path) && tab.path !== '/')
+          }
+          onClick={
+            window.location.pathname === tab.path ||
+            (window.location.pathname.includes(tab.path) && tab.path !== '/')
+              ? () => {}
+              : () => router.push(tab.path)
+          }
         />
       ))}
     </div>
