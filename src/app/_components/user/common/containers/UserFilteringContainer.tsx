c93a5cc0839346b0bc7796816b49db07151814fe
Ginieee <kaj1226@naver.com>
2024-08-06 19:50:04 +0900

    fix: UserFilteringContainer에 bg-white 추가

--- src/app/_components/user/common/containers/UserFilteringContainer.tsx
+++ src/app/_components/user/common/containers/UserFilteringContainer.tsx
@@ -1,18 +1,20 @@
 import { RefreshIcon } from '@/_assets/icons';
 import Image from 'next/image';
 import { ReactNode } from 'react';
+import { UserOrderContainerProps } from './UserOrderContainer';
+import UserFilteringAtom from '../atoms/UserFilteringAtom';
 
 interface UserFilteringContainerProps {
-  children: ReactNode;
+  filterChildren: ReactNode;
   onRefresh: () => void;
 }
 
 const UserFilteringContainer = ({
-  children,
+  filterChildren,
   onRefresh,
 }: UserFilteringContainerProps) => {
   return (
-    <div className="flex w-[350px] flex-col rounded-xl border border-sub-100/50 shadow-[2px_2px_2px_2px_rgba(0,0,0,0.02)]">
+    <div className="flex w-[350px] flex-col rounded-xl border border-sub-100/50 bg-white shadow-[2px_2px_2px_2px_rgba(0,0,0,0.02)]">
       <div className="flex w-full items-center justify-between px-8 py-7">
         <span className="text-1 font-semibold">필터</span>
         <Image
@@ -25,7 +27,7 @@ const UserFilteringContainer = ({
           className="cursor-pointer"
         />
       </div>
-      <div className="flex w-full flex-col">{children}</div>
+      <div className="flex w-full flex-col">{filterChildren}</div>
       <div className="mt-[100px] flex w-full items-center justify-end gap-x-2.5 px-8 py-7">
         {/* 머지 후 버튼 컴포넌트로 대체 필요 */}
         <button>취소</button>
