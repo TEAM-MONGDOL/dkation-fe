7ccc40f30f2765936c1de5d5ed37dbdbaa98008a
Ginieee <kaj1226@naver.com>
2024-08-06 14:40:55 +0900

    fix: HeaderModule에 showLogout 추가

--- src/app/_components/common/modules/HeaderModule.tsx
+++ src/app/_components/common/modules/HeaderModule.tsx
@@ -5,17 +5,21 @@ import LogoutButtonAtom from '@/_components/common/atoms/LogoutButtonAtom';
 import logo from '@/_assets/images/logo_imsy.png';
 import { useRouter } from 'next/navigation';
 
-const HeaderModule = () => {
+interface HeaderModuleProps {
+  showLogout?: boolean;
+}
+
+const HeaderModule = ({ showLogout = true }: HeaderModuleProps) => {
   const router = useRouter();
   return (
-    <div className="flex h-6xl items-center justify-between border-b border-stroke-100 px-5 py-3.5">
+    <div className="flex h-6xl w-full items-center justify-between border-b border-stroke-100 px-5 py-3.5">
       <Image
         className="h-full max-w-24 cursor-pointer object-contain"
         src={logo}
         alt="logo"
         onClick={() => router.push('/admin')}
       />
-      <LogoutButtonAtom />
+      {showLogout && <LogoutButtonAtom />}
     </div>
   );
 };
