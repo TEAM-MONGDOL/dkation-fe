09614ce7982257010ae7716c170b39d687354743
은지 <81403265+eunji0714@users.noreply.github.com>
2024-08-06 16:13:05 +0900

    refactor: 간격 조정

--- src/app/_components/user/common/modules/UserTabBarModule.tsx
+++ src/app/_components/user/common/modules/UserTabBarModule.tsx
@@ -6,7 +6,7 @@ interface UserTabBarModuleProps {
 
 const UserTabBarModule = ({ tabs }: UserTabBarModuleProps) => {
   return (
-    <div className="flex h-[70px] w-full items-center gap-x-6xl border-b-[0.5px] border-sub-100 px-[191px] shadow-[0_1.5px_3px_0_rgba(0,0,0,0.2)]">
+    <div className="flex h-[70px] w-full items-center gap-x-6xl border-b-[0.5px] border-sub-100 px-40 shadow-[0_1.5px_3px_0_rgba(0,0,0,0.2)]">
       {tabs.map((tab, index) => (
         <UserTabBarAtom
           key={tab.text}
