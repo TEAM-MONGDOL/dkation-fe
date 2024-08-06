919ba437c2edbc0e7a3b78633903d8d57f5ed070
은지 <81403265+eunji0714@users.noreply.github.com>
2024-08-06 14:27:51 +0900

    user layout 추가

--- src/app/(route)/(user)/layout.tsx
+++ src/app/(route)/(user)/layout.tsx
@@ -1,11 +1,19 @@
 import React, { ReactNode } from 'react';
+import UserNavBarContainer from '@/_components/user/common/containers/UserNavBarContainer';
+import UserFooterContainer from '@/_components/user/common/containers/UserFooterContainer';
 
 interface Props {
   children: ReactNode;
 }
 
-const layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+const Userlayout = ({ children }: Props) => {
+  return (
+    <div>
+      <UserNavBarContainer />
+      <main className="mb-20 flex-1 overflow-y-auto px-40">{children}</main>
+      <UserFooterContainer />
+    </div>
+  );
 };
 
-export default layout;
+export default Userlayout;
--- src/app/_components/user/common/containers/UserNavBarContainer.tsx
+++ src/app/_components/user/common/containers/UserNavBarContainer.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import logo from '@/_assets/images/logo.png';
 import UserNavButtonAtom from '@/_components/user/common/atoms/UserNavButtonAtom';
+import Link from 'next/link';
 
 const UserNavBarContainer = () => {
   const [path, setPath] = useState('');
@@ -15,17 +16,12 @@ const UserNavBarContainer = () => {
   }, []);
 
   const menuItems = [
-    { href: '/', text: '홈' },
+    { href: '/home', text: '홈' },
     { href: '/workation', text: '워케이션' },
     { href: '/point', text: '포인트' },
     { href: '/support', text: '고객지원' },
     { href: '/mypage', text: '마이페이지' },
   ];
-
-  const handleNavigation = (href: string) => {
-    router.push(href);
-  };
-
   return (
     <div className="mx-28 mt-2 flex h-20 items-center">
       <div className="mr-28 flex">
@@ -38,10 +34,20 @@ const UserNavBarContainer = () => {
             key={item.href}
             className="flex h-20 w-[90px] cursor-pointer flex-col items-center justify-center gap-2"
           >
-            <p className={path === item.href ? 'font-bold' : ''}>{item.text}</p>
-            {path === item.href && (
-              <div className="h-[3px] w-[100px] bg-primary" />
-            )}
+            <Link href={item.href}>
+              <p
+                className={
+                  path === item.href
+                    ? 'text-center font-semibold'
+                    : 'text-center'
+                }
+              >
+                {item.text}
+              </p>
+              {path === item.href && (
+                <div className="h-[3px] w-[100px] translate-y-2 bg-primary" />
+              )}
+            </Link>
           </div>
         ))}
       </div>
