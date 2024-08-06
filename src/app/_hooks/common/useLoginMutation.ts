2944563acb89536a2353a231722a43f076407923
Ginieee <kaj1226@naver.com>
2024-08-07 00:16:02 +0900

    fix: useLoginMutation에 useAuthStore 적용

--- src/app/_hooks/common/useLoginMutation.ts
+++ src/app/_hooks/common/useLoginMutation.ts
@@ -1,4 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
+import useAuthStore from '@/_stores/useAuthStore';
 import api from '../Axios';
 
 export const useLoginMutation = ({
@@ -8,12 +9,15 @@ export const useLoginMutation = ({
   successCallback?: (data: any) => void;
   errorCallback?: (error: Error) => void;
 }) => {
+  const { login } = useAuthStore();
   return useMutation({
     mutationFn: async (request: { accountId: string; password: string }) => {
       const response = await api.post('/api/auth/login', request);
       return response.data;
     },
     onSuccess: (data) => {
+      const result = data.data;
+      login(result.accessToken, result.accountId);
       successCallback && successCallback(data);
     },
     onError: (error: Error) => {
