2f9d798e11148788afdb15b46c1158debaa2d1c2
Ginieee <kaj1226@naver.com>
2024-08-08 01:28:14 +0900

    feat: Session 타입 커스텀

---
+++ next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth/next';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      name: number;
+      sub: string;
+      type: string;
+      token: string;
+      id: string;
+      iat: number;
+      exp: number;
+      jti: string;
+    };
+    accessToken: string;
+    refreshToken: string;
+  }
+}
