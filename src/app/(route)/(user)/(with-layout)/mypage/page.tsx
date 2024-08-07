b7b93089ca0c047a5211075ed32850654f17b110
gahyeon <gus_05@naver.com>
2024-08-07 19:38:07 +0900

    feat: 유저 회원정보>마이페이지 비밀번호 변경 모달 생성

--- src/app/(route)/(user)/(with-layout)/mypage/page.tsx
+++ src/app/(route)/(user)/(with-layout)/mypage/page.tsx
@@ -1,8 +1,95 @@
+'use client';
+
+import React, { useState } from 'react';
 import UserInfosectionModule from '@/_components/user/common/modules/UserInfosectionModule';
 import UserButtonAtom from '@/_components/user/common/atoms/UserButtonAtom';
 import MyPoint from '@/_components/user/mypage/MyPoint';
+import UserModalAtom from '@/_components/user/common/atoms/UserModalAtom';
+import UserModalTitleAtom from '@/_components/user/common/atoms/UserModalTextAtom';
+import UserPasswordInput from '@/_components/user/mypage/UserPasswordInput';
 
 const UserMyPage = () => {
+  const [currentModal, setCurrentModal] = useState<
+    'none' | 'password' | 'newPassword' | 'completed'
+  >('none');
+  const [form, setForm] = useState({
+    password: '',
+    newPassword: '',
+    confirmPassword: '',
+  });
+  const [errors, setErrors] = useState({
+    passwordError: null as string | null,
+    newPasswordError: null as string | null,
+    confirmPasswordError: null as string | null,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const validatePassword = () => {
+    let hasError = false;
+    const newErrors = { ...errors };
+
+    if (form.password.trim() === '') {
+      newErrors.passwordError = '비밀번호를 입력해주세요.';
+      hasError = true;
+    } else {
+      newErrors.passwordError = null;
+    }
+
+    setErrors(newErrors);
+    return !hasError;
+  };
+
+  const validateNewPasswords = () => {
+    let hasError = false;
+    const newErrors = { ...errors };
+
+    if (form.newPassword.trim() === '') {
+      newErrors.newPasswordError = '새 비밀번호를 입력해 주세요.';
+      hasError = true;
+    } else {
+      newErrors.newPasswordError = null;
+    }
+
+    if (form.confirmPassword.trim() === '') {
+      newErrors.confirmPasswordError = '새 비밀번호를 한 번 더 입력해 주세요.';
+      hasError = true;
+    } else if (form.newPassword !== form.confirmPassword) {
+      newErrors.confirmPasswordError = '비밀번호가 일치하지 않습니다.';
+      hasError = true;
+    } else {
+      newErrors.confirmPasswordError = null;
+    }
+
+    setErrors(newErrors);
+    return !hasError;
+  };
+
+  const handleConfirm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (currentModal === 'password') {
+      if (validatePassword()) {
+        setCurrentModal('newPassword');
+      }
+    } else if (currentModal === 'newPassword') {
+      if (validateNewPasswords()) {
+        setCurrentModal('completed');
+      }
+    }
+  };
+
+  const closeModal = () => {
+    setCurrentModal('none');
+    setForm({ password: '', newPassword: '', confirmPassword: '' });
+    setErrors({
+      passwordError: null,
+      newPasswordError: null,
+      confirmPasswordError: null,
+    });
+  };
+
   return (
     <section className="px-40 pt-18">
       <div className="pb-14">
@@ -40,9 +127,107 @@ const UserMyPage = () => {
             buttonStyle="black"
             type="button"
             className="rounded-md"
+            onClick={() => setCurrentModal('password')}
           />
         </UserInfosectionModule>
       </div>
+
+      {currentModal === 'password' && (
+        <UserModalAtom>
+          <UserModalTitleAtom className="text-1 font-semibold">
+            비밀번호 확인
+          </UserModalTitleAtom>
+          <p className="mt-4">현재 비밀번호를 입력하세요</p>
+          <form className="mt-10" onSubmit={handleConfirm}>
+            <UserPasswordInput
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+              placeholder="현재 비밀번호를 입력하세요."
+              error={errors.passwordError}
+            />
+            <div className="flex justify-center gap-x-2 pt-16">
+              <UserButtonAtom
+                text="취소"
+                size="md"
+                buttonStyle="white"
+                type="button"
+                onClick={closeModal}
+                className="rounded-md"
+              />
+              <UserButtonAtom
+                text="확인"
+                size="md"
+                buttonStyle="yellow"
+                type="submit"
+                className="rounded-md"
+              />
+            </div>
+          </form>
+        </UserModalAtom>
+      )}
+
+      {currentModal === 'newPassword' && (
+        <UserModalAtom>
+          <UserModalTitleAtom className="text-1 font-semibold">
+            비밀번호 변경
+          </UserModalTitleAtom>
+          <p className="mt-4">변경할 비밀번호를 입력해주세요.</p>
+          <form className="mt-10" onSubmit={handleConfirm}>
+            <div className="flex flex-col gap-y-1">
+              <UserPasswordInput
+                name="newPassword"
+                value={form.newPassword}
+                onChange={handleChange}
+                placeholder="새 비밀번호를 입력해 주세요."
+                error={errors.newPasswordError}
+              />
+              <UserPasswordInput
+                name="confirmPassword"
+                value={form.confirmPassword}
+                onChange={handleChange}
+                placeholder="새 비밀번호를 한 번 더 입력해 주세요."
+                error={errors.confirmPasswordError}
+              />
+            </div>
+            <div className="flex justify-center gap-x-2 pt-16">
+              <UserButtonAtom
+                text="취소"
+                size="md"
+                buttonStyle="white"
+                type="button"
+                onClick={closeModal}
+                className="rounded-md"
+              />
+              <UserButtonAtom
+                text="확인"
+                size="md"
+                buttonStyle="yellow"
+                type="submit"
+                className="rounded-md"
+              />
+            </div>
+          </form>
+        </UserModalAtom>
+      )}
+
+      {currentModal === 'completed' && (
+        <UserModalAtom>
+          <UserModalTitleAtom className="text-2">
+            비밀번호 변경이 완료되었습니다.
+          </UserModalTitleAtom>
+          <div className="mt-10 flex justify-center">
+            <UserButtonAtom
+              text="확인"
+              size="md"
+              buttonStyle="yellow"
+              type="button"
+              className="rounded-md"
+              onClick={closeModal}
+            />
+          </div>
+        </UserModalAtom>
+      )}
     </section>
   );
 };
