a818eca1be251a07bd706960942d4c76a27153c4
Ginieee <kaj1226@naver.com>
2024-08-08 15:41:37 +0900

    feat: usePostCertificationSend 훅 생성

---
+++ src/app/_hooks/user/usePostCertificationSend.ts
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import api from '../Axios';
+
+export const usePostCertificationSend = ({
+  successCallback,
+  errorCallback,
+}: {
+  successCallback?: () => void;
+  errorCallback?: (error: any) => void;
+}) => {
+  return useMutation({
+    mutationFn: async (data: { email: string }) => {
+      const response = await api.post('/api/member/certification/send', data);
+      return response.data.data;
+    },
+    onSuccess: () => {
+      successCallback && successCallback();
+    },
+    onError: (error) => {
+      errorCallback && errorCallback(error);
+    },
+  });
+};
