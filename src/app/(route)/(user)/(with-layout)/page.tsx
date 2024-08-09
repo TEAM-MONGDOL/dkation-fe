b5157fbeeb067e304923d39748ea5ba011ca6a68
Ginieee <kaj1226@naver.com>
2024-08-09 20:46:19 +0900

    feat: HomePage에 MainHeaderSection 적용

--- src/app/(route)/(user)/(with-layout)/page.tsx
+++ src/app/(route)/(user)/(with-layout)/page.tsx
@@ -1,10 +1,12 @@
 import BannerSection from '@/_components/user/home/BannerSection';
+import MainHeaderSection from '@/_components/user/home/MainHeaderSection';
 import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="flex w-full flex-col">
       <BannerSection />
+      <MainHeaderSection />
     </div>
   );
 };
