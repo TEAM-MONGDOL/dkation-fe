79e0aa6aa935b18b18ff02a71f4af9b57abaf68f
Ginieee <kaj1226@naver.com>
2024-08-09 11:37:40 +0900

    fix: usePostCertification 관련 훅 네이밍 변경

--- src/app/_hooks/user/usePostCertificationCheckMutation.ts
+++ src/app/_hooks/user/usePostCertificationCheckMutation.ts
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import api from '../Axios';
 
-export const usePostCertificationCheck = ({
+export const usePostCertificationCheckMutation = ({
   successCallback,
   errorCallback,
 }: {
