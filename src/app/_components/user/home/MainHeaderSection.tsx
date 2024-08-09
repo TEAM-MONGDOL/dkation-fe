a2dda66fd484b3f44a1a70f659740a966fae21dd
Ginieee <kaj1226@naver.com>
2024-08-09 22:06:19 +0900

    feat: MainHeaderSection 생성

--- src/app/_components/user/home/MainHeaderSection.tsx
+++ src/app/_components/user/home/MainHeaderSection.tsx
@@ -8,8 +8,8 @@ import UserButtonAtom from '../common/atoms/UserButtonAtom';
 const MainHeaderSection = () => {
   const router = useRouter();
   return (
-    <section className="flex h-[633px] w-full flex-col bg-primary px-28">
-      <div className="bg-home-bg flex h-full flex-col items-start justify-between bg-center bg-no-repeat object-cover px-40 pb-28 pt-36 text-sub-400">
+    <section className="flex h-[633px] w-full flex-col bg-primary px-40">
+      <div className="bg-home-bg flex h-full flex-col items-start justify-between bg-center bg-no-repeat object-cover pb-28 pt-36 text-sub-400">
         <div className="flex flex-col gap-y-4">
           <Image src={LogoTextIcon} alt="home-logo" width={184} />
           <p className="text-h1 font-semibold">
