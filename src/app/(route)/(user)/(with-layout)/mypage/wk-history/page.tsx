e18056da7750c91ca8271e266141aea26a28569e
gahyeon <gus_05@naver.com>
2024-08-10 03:37:24 +0900

    fix: 모달 관련 코드 삭제

--- src/app/(route)/(user)/(with-layout)/mypage/wk-history/page.tsx
+++ src/app/(route)/(user)/(with-layout)/mypage/wk-history/page.tsx
@@ -2,14 +2,12 @@
 
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useGetNoticeListQuery } from '@/_hooks/admin/useGetNoticeListQuery';
 import dayjs from 'dayjs';
 import UserFilteringSectionContainer from '@/_components/user/common/containers/UserFilteringSectionContainer';
 import UserStateFilteringContainer from '@/_components/user/common/containers/UserStateFilteringContainer';
 import UserPlaceFilteringContainer from '@/_components/user/common/containers/UserPlaceFilteringContainer';
 import WorkationCard from '@/_components/user/mypage/UserWktCard';
-import place from '@/_assets/images/place_impy.png';
-import UserWktConfirmModal from '@/_components/user/mypage/UserWktConfirmModal';
+import { useGetMyWktHistoryQuery } from '@/_hooks/user/useGetMyWktHistoryQuery';
 
 const UserWkHistoryPage = () => {
   const router = useRouter();
@@ -18,10 +16,19 @@ const UserWkHistoryPage = () => {
   const [endDate, setEndDate] = useState<Date | null>(null);
   const [param, setParam] = useState<{
     order: string;
-    noticeType: string[];
+    type: string[];
   }>({
     order: 'DESC',
-    noticeType: ['ANNOUNCEMENT', 'RESULT', 'EVENT'],
+    type: [
+      'APPLIED',
+      'RAFFLE_WAIT',
+      'NO_WINNING',
+      'CONFIRM_WAIT',
+      'CANCEL',
+      'CONFIRM',
+      'WAIT',
+      'VISITED',
+    ],
   });
 
   const [isFilteringSectionOpen, setIsFilteringSectionOpen] = useState<
@@ -30,12 +37,8 @@ const UserWkHistoryPage = () => {
   const [selectedState, setSelectedState] = useState<string>('ALL');
   const [selectedOrder, setSelectedOrder] = useState<string>('createdAt,DESC');
   const [selectedSpace, setSelectedSpace] = useState<string[]>(['양양 쏠비치']);
-  const [openModal, setOpenModal] = useState<
-    'CANCEL' | 'CONFIRM' | 'CANCELLATION_CONFIRMATION' | 'APPLIED_CANCEL' | null
-  >(null);
 
-  const { data, isLoading, isError } = useGetNoticeListQuery({
-    types: param.noticeType.join(','),
+  const { data, isLoading, isError } = useGetMyWktHistoryQuery({
     startDate: startDate
       ? dayjs(startDate).format('YYYY-MM-DDTHH:mm:ss')
       : undefined,
@@ -50,24 +53,17 @@ const UserWkHistoryPage = () => {
   const handleCardClick = (applyStatusType: string) => {
     switch (applyStatusType) {
       case 'APPLIED':
-        setOpenModal('APPLIED_CANCEL'); // APPLIED 상태에서는 CancelModal을 표시하도록 설정
+        console.log('신청  취소하기 클릭');
         break;
       case 'CONFIRM_WAIT':
-        setOpenModal('CONFIRM');
+        console.log('방문 확정하기 클릭');
         break;
       case 'VISITED':
         router.push('/mypage/review/new');
         break;
       default:
+        break;
     }
-  };
-
-  const handleCancelConfirm = () => {
-    setOpenModal('CANCELLATION_CONFIRMATION');
-  };
-
-  const handleConfirmConfirm = () => {
-    // 수락 로직 처리
   };
 
   return (
@@ -117,82 +113,28 @@ const UserWkHistoryPage = () => {
             }}
           />
         </div>
-        <WorkationCard
-          thumbnailUrl={place.src}
-          wktName="2024년 7월 3주차 워케이션"
-          place="양양"
-          totalRecruit={2}
-          applyStartDate="2024.07.01"
-          applyEndDate="2024.07.15"
-          startDate="2024.07.10"
-          endDate="2024.07.12"
-          bettingPoint={400}
-          applyStatusType="CONFIRM_WAIT"
-          waitingNumber={4}
-          onClick={handleCardClick}
-        />
-        <WorkationCard
-          thumbnailUrl={place.src}
-          wktName="2024년 7월 3주차 워케이션"
-          place="양양"
-          totalRecruit={2}
-          applyStartDate="2024.07.01"
-          applyEndDate="2024.07.15"
-          startDate="2024.07.10"
-          endDate="2024.07.12"
-          bettingPoint={400}
-          applyStatusType="APPLIED"
-          waitingNumber={4}
-          onClick={handleCardClick}
-        />
-        <WorkationCard
-          thumbnailUrl={place.src}
-          wktName="2024년 7월 3주차 워케이션"
-          place="양양"
-          totalRecruit={2}
-          applyStartDate="2024.07.01"
-          applyEndDate="2024.07.15"
-          startDate="2024.07.10"
-          endDate="2024.07.12"
-          bettingPoint={400}
-          applyStatusType="VISITED"
-          waitingNumber={4}
-          onClick={handleCardClick}
-        />
-        <WorkationCard
-          thumbnailUrl={place.src}
-          wktName="2024년 7월 3주차 워케이션"
-          place="양양"
-          totalRecruit={2}
-          applyStartDate="2024.07.01"
-          applyEndDate="2024.07.15"
-          startDate="2024.07.10"
-          endDate="2024.07.12"
-          bettingPoint={400}
-          applyStatusType="NO_WINNING"
-          waitingNumber={4}
-          onClick={handleCardClick}
-        />
+        {data?.applyInfoList.length === 0 ? (
+          <p>등록된 워케이션이 없습니다.</p>
+        ) : (
+          data?.applyInfoList.map((wkt) => (
+            <WorkationCard
+              key={wkt.wktName}
+              thumbnailUrl={wkt.thumbnailUrl}
+              wktName={wkt.wktName}
+              place={wkt.place}
+              totalRecruit={wkt.totalRecruit}
+              applyStartDate={wkt.applyStartDate}
+              applyEndDate={wkt.applyEndDate}
+              startDate={wkt.startDate}
+              endDate={wkt.endDate}
+              bettingPoint={wkt.bettingPoint}
+              applyStatusType={wkt.applyStatusType}
+              waitingNumber={4} // 추후 수정 필요
+              onClick={handleCardClick}
+            />
+          ))
+        )}
       </div>
-
-      {openModal && (
-        <UserWktConfirmModal
-          onClose={() => setOpenModal(null)}
-          onConfirm={handleConfirmConfirm}
-          onCancel={handleCancelConfirm}
-          modalType={
-            openModal === 'CANCEL'
-              ? 'cancel'
-              : openModal === 'CONFIRM'
-                ? 'confirm'
-                : openModal === 'CANCELLATION_CONFIRMATION'
-                  ? 'cancellationConfirmation'
-                  : openModal === 'APPLIED_CANCEL'
-                    ? 'cancel'
-                    : 'acceptConfirmation' // 여기서 추가할 경우, 모든 경우를 포괄합니다.
-          }
-        />
-      )}
     </section>
   );
 };
