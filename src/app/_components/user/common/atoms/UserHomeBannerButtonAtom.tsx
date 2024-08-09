4e955ba2008e666631f27b574cd7570907b71295
Ginieee <kaj1226@naver.com>
2024-08-09 13:34:01 +0900

    fix: UserHomeBannerButtonAtom에서 배경, 글자 색 받도록 수정

--- src/app/_components/user/common/atoms/UserHomeBannerButtonAtom.tsx
+++ src/app/_components/user/common/atoms/UserHomeBannerButtonAtom.tsx
@@ -2,11 +2,19 @@ import Link from 'next/link';
 
 export interface HomeBannerProps {
   link: string;
+  buttonBgColor: string;
+  buttonTextColor: string;
 }
-const UserHomeBannerButtonAtom = ({ link }: HomeBannerProps) => {
+const UserHomeBannerButtonAtom = ({
+  link,
+  buttonBgColor,
+  buttonTextColor,
+}: HomeBannerProps) => {
   return (
     <Link href={link}>
-      <button className="h-7 w-16 items-center justify-center rounded-[17px] bg-primary text-5 text-black">
+      <button
+        className={`h-7 w-16 items-center justify-center rounded-[17px] ${buttonBgColor} text-5 ${buttonTextColor}`}
+      >
         확인하기
       </button>
     </Link>
