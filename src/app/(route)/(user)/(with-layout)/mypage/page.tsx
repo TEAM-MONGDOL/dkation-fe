c4e5b0860609e3209965a7e03c236bba3516d88f
gahyeon <gus_05@naver.com>
2024-08-10 01:00:52 +0900

    refactor: useGetMemberDetailQuery 훅 common으로 이동

--- src/app/(route)/(user)/(with-layout)/mypage/page.tsx
+++ src/app/(route)/(user)/(with-layout)/mypage/page.tsx
@@ -8,7 +8,7 @@ import UserModalAtom from '@/_components/user/common/atoms/UserModalAtom';
 import UserModalTitleAtom from '@/_components/user/common/atoms/UserModalTextAtom';
 import UserPasswordInput from '@/_components/user/mypage/UserPasswordInput';
 import { useSession } from 'next-auth/react';
-import { useGetMemberDetailQuery } from '@/_hooks/admin/useGetMemberDetailQuery';
+import { useGetMemberDetailQuery } from '@/_hooks/common/useGetMemberDetailQuery';
 
 const UserMyPage = () => {
   const session = useSession();
