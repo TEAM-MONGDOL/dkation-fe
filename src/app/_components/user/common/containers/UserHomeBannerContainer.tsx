09c277969c4bb96916f7418f114effa898a2ba40
Ginieee <kaj1226@naver.com>
2024-08-09 13:34:54 +0900

    fix: UserHomeBannerContainer에서 배경색 받도록 수정

--- src/app/_components/user/common/containers/UserHomeBannerContainer.tsx
+++ src/app/_components/user/common/containers/UserHomeBannerContainer.tsx
@@ -4,13 +4,23 @@ import UserHomeBannerButtonAtom from '@/_components/user/common/atoms/UserHomeBa
 export interface HomeBannerProps {
   title: string;
   link: string;
+  bgColor: string;
 }
 
-const UserHomeBannerContainer = ({ title, link }: HomeBannerProps) => {
+const UserHomeBannerContainer = ({ title, link, bgColor }: HomeBannerProps) => {
   return (
-    <div className="flex h-[74px] items-center justify-center gap-7 bg-button text-white">
-      <UserHomeBannerTitleAtom title={title} />
-      <UserHomeBannerButtonAtom link={link} />
+    <div
+      className={`flex h-[74px] items-center justify-center gap-7 ${bgColor} text-white`}
+    >
+      <UserHomeBannerTitleAtom
+        title={title}
+        textColor={bgColor === 'DARK' ? 'text-white' : 'text-black'}
+      />
+      <UserHomeBannerButtonAtom
+        link={link}
+        buttonBgColor={bgColor === 'DARK' ? 'bg-primary' : 'bg-sub-400'}
+        buttonTextColor={bgColor === 'DARK' ? 'text-black' : 'text-white'}
+      />
     </div>
   );
 };
