8c09d70086e7e193ad0b3a70818406d54bfa28eb
Ginieee <kaj1226@naver.com>
2024-08-10 00:41:40 +0900

    add: tailwind.config에 cus-400 컬러 추가 (#F9F9F9)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,7 @@ const config: Config = {
         'cus-100': '#F4F6FA',
         'cus-200': '#374553',
         'cus-300': '#323232',
+        'cus-400': '#F9F9F9',
         'stroke-100': '#DBDBDB',
         footer: '#888888',
         button: '#2A2A2A',
