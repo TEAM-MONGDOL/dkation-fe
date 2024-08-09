f05c127fa0fd1c29fcd960261fccf3d377cbc445
은지 <81403265+eunji0714@users.noreply.github.com>
2024-08-09 14:41:23 +0900

    feat: 워케이션 신청페이지 완성

--- src/app/(route)/(user)/(with-layout)/workation/[id]/apply/page.tsx
+++ src/app/(route)/(user)/(with-layout)/workation/[id]/apply/page.tsx
@@ -2,12 +2,13 @@ import place from '@/_assets/images/place_impy.png';
 import WkBannerPeriodModule from '@/_components/user/common/modules/WkBannerPeriodModule';
 import Image from 'next/image';
 import { LocationIcon } from '@/_assets/icons';
+import UserButtonAtom from '@/_components/user/common/atoms/UserButtonAtom';
 
 const UserWkApplyPage = () => {
   return (
     <section>
       <div className="">
-        <div className="flex h-[450px] justify-center bg-primary/5 px-40 py-12">
+        <div className="flex h-[450px] justify-center bg-primary/5 px-36 py-12">
           <Image src={place} alt="place" width={630} />
           <div className="ml-auto pr-8 pt-56">
             <p className="text-2">2024년 7월 3주차 워케이션</p>
@@ -27,12 +28,29 @@ const UserWkApplyPage = () => {
           endDate="2020.07.14"
         />
       </div>
-      <div className="px-52">
+      <div className="mb-48 mt-28 flex items-center justify-between px-60">
         <div>
-          <p className="text-h2 font-semibold">나의 당첨 확률</p>
-          <div className="flex w-[348px] items-end justify-center gap-10 rounded-[17px] border border-[#E5CD07] bg-gradient-to-r from-[#E5CD07]/5 to-[#E5CD07]/60 pb-2.5">
+          <p className="mb-10 text-h2 font-semibold">나의 당첨 확률</p>
+          <div className="flex w-[348px] items-end justify-center gap-10 rounded-[17px] border border-[#E5CD07] bg-gradient-to-r from-primary/5 to-primary/60 pb-2.5">
             <p className="text-[78px] font-bold">2.1%</p>
             <p className="mb-5 text-h3 text-[#E5CD07]">±N%</p>
+          </div>
+        </div>
+        <div className="mt-16 flex flex-col gap-2">
+          <div className="flex items-center gap-12">
+            <p className="text-1">내 포인트</p>
+            <input className="h-12 rounded-[4px] border pl-4" placeholder="0" />
+          </div>
+          <div className="flex items-center gap-7">
+            <p className="text-1">배팅 포인트</p>
+            <input className="h-12 rounded-[4px] border pl-4" placeholder="0" />
+            <UserButtonAtom
+              className="rounded-[8px]"
+              text="신청하기"
+              size="md"
+              buttonStyle="black"
+              type="submit"
+            />
           </div>
         </div>
       </div>
