6dada3439e7e0c22ceb695925c6932103d35cd36
Ginieee <kaj1226@naver.com>
2024-08-09 20:18:30 +0900

    feat: BannerSection 구현 완료

--- src/app/_components/user/home/BannerSection.tsx
+++ src/app/_components/user/home/BannerSection.tsx
@@ -1,13 +1,44 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import { useGetHomeBannerQuery } from '@/_hooks/user/useGetHomeBannerQuery';
-import React from 'react';
+import UserHomeBannerContainer from '../common/containers/UserHomeBannerContainer';
 
 const BannerSection = () => {
   const { data } = useGetHomeBannerQuery();
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(data);
-  return <div>배너 섹션</div>;
+  // Automatically slide to the next banner every 5 seconds
+  useEffect(() => {
+    if (!data || data.bannerInfoList.length < 1) return;
+
+    const intervalId = setInterval(() => {
+      setCurrentIndex(
+        (prevIndex) => (prevIndex + 1) % data.bannerInfoList.length,
+      );
+    }, 5000); // 5000ms = 5 seconds
+
+    /* eslint-disable consistent-return */
+    return () => clearInterval(intervalId);
+  }, [data]);
+
+  return (
+    <div className="relative w-full overflow-hidden">
+      <div
+        className="flex shrink-0 transition-transform duration-500"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {data?.bannerInfoList.map((bannerInfo) => (
+          <UserHomeBannerContainer
+            key={bannerInfo.id}
+            title={bannerInfo.title}
+            link={bannerInfo.linkUrl}
+            bgColor={bannerInfo.backgroundColor}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BannerSection;
