9b9857898f55475938f793a4492d059b92a4991a
gahyeon <gus_05@naver.com>
2024-08-09 22:34:58 +0900

    feat: 배너 등록시 연결할 공지사항 선택할 수 있도록 드롭다운 추가

--- src/app/(route)/admin/(with-layout)/notices/banner/new/page.tsx
+++ src/app/(route)/admin/(with-layout)/notices/banner/new/page.tsx
@@ -1,21 +1,34 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import TitleBarModule from '@/_components/common/modules/TitleBarModule';
 import InputModule from '@/_components/common/modules/InputModule';
 import ButtonAtom from '@/_components/common/atoms/ButtonAtom';
 import ColorChartModule from '@/_components/admin/notices/StyleChartModule';
+import DropdownModule from '@/_components/common/modules/DropdownModule';
+import { useGetNoticeListQuery } from '@/_hooks/admin/useGetNoticeListQuery';
 
-const WriteNoticesPage = () => {
-  const router = useRouter();
+const AddBannerPage = () => {
   const [values, setValues] = useState({
     title: '',
     linkUrl: '',
     backgroundColor: '',
   });
 
-  const handleSelect = (option: string) => {};
+  const { data } = useGetNoticeListQuery({
+    pageParam: { page: 1, size: 100 },
+  });
+
+  const noticeInfos = data?.announcementInfos || [];
+  const noticeOptions = noticeInfos.map((notice) => ({
+    id: notice.id,
+    title: notice.title,
+  }));
+
+  const findIdByTitle = (title: string) => {
+    const notice = noticeOptions.find((option) => option.title === title);
+    return notice ? notice.id.toString() : '';
+  };
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
@@ -30,47 +43,77 @@ const WriteNoticesPage = () => {
     setValues({ ...values, backgroundColor: color });
   };
 
+  const handleNoticeSelect = (selectedTitle: string) => {
+    const id = findIdByTitle(selectedTitle);
+    setValues({
+      ...values,
+      linkUrl: id,
+    });
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    console.log('제목:', values.title);
+    console.log('링크:', values.linkUrl);
+    console.log('배경색:', values.backgroundColor);
     const payload = { ...values };
   };
 
   return (
     <section>
       <TitleBarModule title="배너 추가하기" type="LEFT" />
-      <form onSubmit={handleSubmit}>
-        <div className="pt-10">
+      <form onSubmit={handleSubmit} className="">
+        <div className="w-3/5 pt-10">
           <div className="mb-3 flex items-center gap-x-3">
             <p className="text-3 font-bold">제목</p>
-            <p className="text-4 text-sub-100">
+            <p className="text-4 text-sub-200/70">
               해당 필드의 입력값이 사용자 메인 페이지에 노출됩니다.
             </p>
           </div>
           <div className="flex w-full">
             <InputModule
               name="title"
-              placeholder="제목을 입력하세요."
-              textCount={50}
+              placeholder="제목을 입력하세요"
+              textCount={20}
               value={values.title}
               onChange={handleChange}
             />
           </div>
-          <p className="mb-3 mt-6 text-3 font-bold">링크</p>
-          {/*  드롭다운 추가 */}
+
+          <div className="mb-3 mt-10 flex items-center gap-x-3">
+            <p className="text-3 font-bold">링크</p>
+            <p className="text-4 text-sub-200/70">
+              선택한 공지사항의 링크가 배너 클릭 시 이동할 URL로 설정됩니다.
+            </p>
+          </div>
           <div>
-            <p className="mb-3 mt-6 text-3 font-bold">스타일</p>
+            <DropdownModule
+              options={noticeOptions.map((option) => option.title)} // 제목을 표시합니다.
+              onSelect={handleNoticeSelect}
+              placeholder="공지사항을 선택하세요"
+              selectedOption={
+                noticeOptions.find(
+                  (option) => option.id.toString() === values.linkUrl,
+                )?.title || ''
+              }
+            />
+          </div>
+
+          <div className="mt-10">
+            <p className="mb-3 text-3 font-bold">스타일</p>
             <ColorChartModule
               selectedColor={values.backgroundColor}
               onSelectColor={handleColorSelect}
             />
           </div>
-          <div className="flex justify-end pt-14">
-            <ButtonAtom buttonStyle="yellow" text="등록" type="submit" />
-          </div>
+        </div>
+        <div className="flex justify-end pt-16">
+          <ButtonAtom buttonStyle="yellow" text="등록" type="submit" />
         </div>
       </form>
     </section>
   );
 };
 
-export default WriteNoticesPage;
+export default AddBannerPage;
