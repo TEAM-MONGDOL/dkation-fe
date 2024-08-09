eb4ed87265c7e2b6af90a1772807bf0559061894
Ginieee <kaj1226@naver.com>
2024-08-09 16:40:24 +0900

    feat: useGetHomeBannerQuery 생성

---
+++ src/app/_hooks/user/useGetHomeBannerQuery.ts
@@ -0,0 +1,15 @@
+import { bannerInfoListSchema } from '@/_types/adminType';
+import { useQuery } from '@tanstack/react-query';
+import api from '../Axios';
+
+export const useGetHomeBannerQueryKey = 'useHomeBannerQuery';
+
+export const useGetHomeBannerQuery = () => {
+  return useQuery({
+    queryKey: [useGetHomeBannerQueryKey],
+    queryFn: async () => {
+      const response = await api.get('/api/banner');
+      return bannerInfoListSchema.parse(response.data);
+    },
+  });
+};
