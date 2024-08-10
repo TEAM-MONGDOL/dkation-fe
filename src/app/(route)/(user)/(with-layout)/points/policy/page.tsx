ff4d9d14d5ebe0f339114d30aa5ba4182eb4c8b1
Ginieee <kaj1226@naver.com>
2024-08-11 00:42:07 +0900

    feat: 포인트 정책 페이지 구현 (레이아웃&API)

--- src/app/(route)/(user)/(with-layout)/points/policy/page.tsx
+++ src/app/(route)/(user)/(with-layout)/points/policy/page.tsx
@@ -1,87 +1,104 @@
 'use client';
 
 import EmptyContainer from '@/_components/common/containers/EmptyContainer';
-import UserTableHeaderAtom from '@/_components/user/common/atoms/UserTableHeaderAtom';
-import UserTableBodyAtom from '@/_components/user/common/atoms/UserTableBodyAtom';
-import UserTableBodyModule from '@/_components/user/common/modules/UserTableBodyModule';
-import UserTableContainer from '@/_components/user/common/containers/UserTableContainer';
-import UserTableHeaderModule from '@/_components/user/common/modules/UserTableHeaderModule';
 import { useGetPointPolicyQuery } from '@/_hooks/admin/useGetPointPolicyQuery';
 import { useState } from 'react';
 import Image from 'next/image';
 import { DownArrowIcon } from '@/_assets/icons';
 import { dateConverter } from '@/_types/converter';
 import PaginationModule from '@/_components/common/modules/PaginationModule';
+import { useGetPointPolicyDetailQuery } from '@/_hooks/admin/useGetPointPolicyDetailQuery';
+import { useGetPointPolicyDetailListQuery } from '@/_hooks/user/useGetPointsPolicyDetailListQuery';
 
 const PointsPolicyPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const { data, isLoading, isError } = useGetPointPolicyQuery({
+  const [currentOpen, setCurrentOpen] = useState<number | null>(null);
+  const {
+    data: policyList,
+    isLoading: policyListIsLoading,
+    isError: policyListIsError,
+  } = useGetPointPolicyQuery({
     pageable: {
       page: currentPage,
       size: 5,
       sort: 'lastModifiedAt,DESC',
     },
   });
+  const policyDetailList = useGetPointPolicyDetailListQuery({
+    ids: policyList?.pointPolicyList.map((item) => item.id) || [],
+    enable: policyList !== undefined,
+  });
+
+  const getPolicyDetail = (title: string) => {
+    const detail = policyDetailList.find(
+      (detail) => detail.data?.policyTitle === title,
+    );
+    return detail && detail.data ? detail.data.detail : '';
+  };
+
   return (
     <section className="flex w-full flex-col gap-y-14 px-40 pb-20 pt-18">
       <h2 className="text-h2 font-semibold text-sub-400">포인트 정책</h2>
       <div className="flex w-full flex-col gap-y-[200px]">
         <div className="flex w-full flex-col gap-y-2.5">
-          <div className="flex h-10 w-full items-center justify-center gap-x-7 bg-sub-100/20 px-6 text-center text-4 text-sub-300">
-            <p className="w-[60px]">번호</p>
+          <div className="flex h-10 w-full items-center justify-center gap-x-7 rounded bg-sub-100/20 px-[68px] text-center text-4 text-sub-300">
+            <p className="w-[100px]">번호</p>
             <p className="grow">분류</p>
-            <p className="w-40">점수</p>
-            <p className="w-36">등록 일시</p>
+            <p className="w-60">점수</p>
+            <p className="w-48">등록 일시</p>
+            <p className="w-10" />
           </div>
-        </div>
-        <UserTableContainer>
-          <UserTableHeaderModule>
-            <UserTableHeaderAtom isFirst text="번호" width="200px" />
-            <UserTableHeaderAtom text="분류" />
-            <UserTableHeaderAtom text="점수" width="200px" />
-            <UserTableHeaderAtom text="등록 일시" width="250px" />
-            <UserTableHeaderAtom isLast text="" width="100px" />
-          </UserTableHeaderModule>
-          <tbody>
-            {!data ? (
-              isLoading ? (
-                <EmptyContainer colSpan={5} text="로딩 중..." />
-              ) : isError ? (
-                <EmptyContainer colSpan={5} text="에러 발생" />
+          <div className="flex w-full flex-col gap-y-2.5">
+            {!policyList ? (
+              policyListIsLoading ? (
+                <EmptyContainer text="로딩 중..." notTable />
+              ) : policyListIsError ? (
+                <EmptyContainer text="에러 발생" notTable />
               ) : (
-                <EmptyContainer colSpan={5} text="데이터 없음" />
+                <EmptyContainer text="데이터 없음" notTable />
               )
-            ) : data.pointPolicyList.length < 1 ? (
-              <EmptyContainer colSpan={5} />
+            ) : policyList.pointPolicyList.length < 1 ? (
+              <EmptyContainer notTable />
             ) : (
-              data.pointPolicyList.map((item, idx) => (
-                <UserTableBodyModule key={item.id}>
-                  <UserTableBodyAtom isFirst>
-                    {(currentPage - 1) * 5 + idx + 1}
-                  </UserTableBodyAtom>
-                  <UserTableBodyAtom>{item.policyTitle}</UserTableBodyAtom>
-                  <UserTableBodyAtom>
-                    {item.quantity.toLocaleString()}
-                  </UserTableBodyAtom>
-                  <UserTableBodyAtom>
-                    {dateConverter(item.modifiedAt)}
-                  </UserTableBodyAtom>
-                  <UserTableBodyAtom isLast>
-                    <Image
-                      src={DownArrowIcon}
-                      alt="downArrow"
-                      className="ml-2 transition-transform duration-300"
-                    />
-                  </UserTableBodyAtom>
-                </UserTableBodyModule>
+              policyList.pointPolicyList.map((item, idx) => (
+                <div className="flex w-full flex-col" key={item.policyTitle}>
+                  <div
+                    key={item.id}
+                    className="flex h-[63px] w-full cursor-pointer items-center justify-center gap-x-7 rounded border border-stroke-100 bg-white px-[68px] text-center font-medium text-sub-400"
+                    onClick={() => {
+                      setCurrentOpen(currentOpen === item.id ? null : item.id);
+                    }}
+                  >
+                    <p className="w-[100px]">
+                      {(currentPage - 1) * 5 + idx + 1}
+                    </p>
+                    <p className="grow">{item.policyTitle}</p>
+                    <p className="w-60">{item.quantity.toLocaleString()}</p>
+                    <p className="w-48">{dateConverter(item.modifiedAt)}</p>
+                    <div className="w-10">
+                      <Image
+                        src={DownArrowIcon}
+                        alt="policy-toggle"
+                        className={`transition-transform duration-300 ${currentOpen === item.id ? 'rotate-180' : ''}`}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className={`transition-max-height overflow-hidden duration-500 ease-in-out ${currentOpen === item.id ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'} w-full`}
+                  >
+                    <div className="my-2.5 w-full rounded bg-sub-100/10 px-6 py-7 text-sub-300">
+                      {getPolicyDetail(item.policyTitle)}
+                    </div>
+                  </div>
+                </div>
               ))
             )}
-          </tbody>
-        </UserTableContainer>
-        {data && data.pageInfo.totalElements > 0 && (
+          </div>
+        </div>
+        {policyList && policyList.pageInfo.totalElements > 0 && (
           <div className="flex w-full items-center justify-center">
             <PaginationModule
-              totalPages={data.pageInfo.totalPages}
+              totalPages={policyList.pageInfo.totalPages}
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
               user
