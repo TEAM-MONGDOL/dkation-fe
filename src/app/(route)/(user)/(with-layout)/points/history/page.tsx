737ea09e2d7c6cbc011661491d61f5e5c82ca8a5
Ginieee <kaj1226@naver.com>
2024-08-10 11:38:16 +0900

    feat: 포인트 사용내역 레이아웃 구현

--- src/app/(route)/(user)/(with-layout)/points/history/page.tsx
+++ src/app/(route)/(user)/(with-layout)/points/history/page.tsx
@@ -1,5 +1,40 @@
+'use client';
+
+import EmptyContainer from '@/_components/common/containers/EmptyContainer';
+import PaginationModule from '@/_components/common/modules/PaginationModule';
+import UserTableHeaderAtom from '@/_components/user/common/atoms/UserTableHeaderAtom';
+import UserTableContainer from '@/_components/user/common/containers/UserTableContainer';
+import UserTableHeaderModule from '@/_components/user/common/modules/UserTableHeaderModule';
+import { useState } from 'react';
+
 const PointsHistoryPage = () => {
-  return <div>포인트 사용내역</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <div className="flex w-full flex-col gap-y-14 px-40 pb-20 pt-18">
+      <h2 className="text-h2 font-semibold text-sub-400">포인트 사용 내역</h2>
+      <div className="flex w-full flex-col gap-y-[200px]">
+        <UserTableContainer>
+          <UserTableHeaderModule>
+            <UserTableHeaderAtom isFirst text="번호" width="200px" />
+            <UserTableHeaderAtom text="사유" />
+            <UserTableHeaderAtom text="적립/사용" width="300px" />
+            <UserTableHeaderAtom isLast text="잔여 포인트" width="200px" />
+          </UserTableHeaderModule>
+          <tbody>
+            <EmptyContainer colSpan={4} />
+          </tbody>
+        </UserTableContainer>
+        <div className="flex w-full items-center justify-center">
+          <PaginationModule
+            totalPages={10}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            user
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PointsHistoryPage;
