c722f8819974ff7a6a8751a8ba1eecb51d82a29c
은지 <81403265+eunji0714@users.noreply.github.com>
2024-08-11 01:50:28 +0900

    feat: 워케이션 신청페이지에서 상세정보 불러오기

--- src/app/(route)/(user)/(with-layout)/workation/[id]/apply/page.tsx
+++ src/app/(route)/(user)/(with-layout)/workation/[id]/apply/page.tsx
@@ -9,6 +9,8 @@ import { useGetUserPercentQuery } from '@/_hooks/user/useGetUserPercentQuery';
 import { useState } from 'react';
 import { useGetMemberDetailQuery } from '@/_hooks/common/useGetMemberDetailQuery';
 import { useSession } from 'next-auth/react';
+import { useGetUserWkDetailQuery } from '@/_hooks/user/useGetUserWkDetailQuery';
+import dayjs from 'dayjs';
 
 interface Props {
   params: { id: number };
@@ -34,6 +36,10 @@ const UserWkApplyPage = ({ params }: Props) => {
     refetch,
   } = useGetUserPercentQuery({ wktId: id, point });
 
+  const { data, isLoading, isError } = useGetUserWkDetailQuery({
+    wktId: id,
+  });
+
   const handlePointChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     const numericValue = Number(value);
@@ -58,33 +64,31 @@ const UserWkApplyPage = ({ params }: Props) => {
       <div className="">
         <div className="flex h-[450px] justify-center bg-primary/5 px-36 py-12">
           <Image src={place} alt="place" width={630} />
-          <div className="ml-auto pr-8 pt-56">
-            <p className="text-2">2024년 7월 3주차 워케이션</p>
-            <p className="mb-4 text-h1 font-semibold">양양</p>
+          <div className="ml-auto pr-16 pt-52">
+            <p className="text-2">{data?.title}</p>
+            <p className="mb-4 text-h1 font-semibold">{data?.place}</p>
             <div className="flex">
               <Image src={LocationIcon} alt="LocationIcon" />
-              <p className="text-2">
-                주소 : 강원 양양군 현북면 하조대해안길 119
-              </p>
+              <p className="text-2">주소 : {data?.address}</p>
             </div>
           </div>
         </div>
         <WkBannerPeriodModule
-          applyStartDate="2020.07.14"
-          applyEndDate="2020.07.14"
-          startDate="2020.07.14"
-          endDate="2020.07.14"
+          applyStartDate={dayjs(data?.applyStartDate).format('YYYY.MM.DD')}
+          applyEndDate={dayjs(data?.applyEndDate).format('YYYY.MM.DD')}
+          startDate={dayjs(data?.startDate).format('YYYY.MM.DD')}
+          endDate={dayjs(data?.endDate).format('YYYY.MM.DD')}
         />
       </div>
       <div className="mb-48 mt-28 flex items-center justify-between px-60">
         <div>
           <p className="mb-10 text-h2 font-semibold">나의 당첨 확률</p>
-          <div className="flex w-[348px] items-end justify-center gap-10 rounded-[17px] border border-[#E5CD07] bg-gradient-to-r from-primary/5 to-primary/60 pb-2.5">
+          <div className="flex items-end justify-center gap-10 rounded-[17px] border border-[#E5CD07] bg-gradient-to-r from-primary/5 to-primary/60 px-9 pb-2.5">
             <p className="text-[78px] font-bold">{pointData?.percentage}%</p>
             <p className="mb-5 text-h3 text-[#E5CD07]">±{pointData?.error}%</p>
           </div>
         </div>
-        <div className="mt-16 flex flex-col gap-2">
+        <div className="mt-20 flex flex-col gap-2">
           <div className="flex items-center gap-12">
             <p className="text-1">내 포인트</p>
             <input
