f3930accc6d9c375737b51b93e210ec05dea4425
Ginieee <kaj1226@naver.com>
2024-08-10 12:11:29 +0900

    feat: 포인트 관리 > 포인트 사용 내역 api 연결 (사용 일시 추가)

--- src/app/(route)/(user)/(with-layout)/points/history/page.tsx
+++ src/app/(route)/(user)/(with-layout)/points/history/page.tsx
@@ -2,13 +2,29 @@
 
 import EmptyContainer from '@/_components/common/containers/EmptyContainer';
 import PaginationModule from '@/_components/common/modules/PaginationModule';
+import UserTableBodyAtom from '@/_components/user/common/atoms/UserTableBodyAtom';
 import UserTableHeaderAtom from '@/_components/user/common/atoms/UserTableHeaderAtom';
+import UserTextLabelAtom from '@/_components/user/common/atoms/UserTextLabelAtom';
 import UserTableContainer from '@/_components/user/common/containers/UserTableContainer';
+import UserTableBodyModule from '@/_components/user/common/modules/UserTableBodyModule';
 import UserTableHeaderModule from '@/_components/user/common/modules/UserTableHeaderModule';
+import { useGetMemberPointHistoryQuery } from '@/_hooks/admin/useGetMemberPointHistoryQuery';
+import { dateConverter } from '@/_types/converter';
+import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 
 const PointsHistoryPage = () => {
+  const session = useSession();
   const [currentPage, setCurrentPage] = useState(1);
+  const { data, isLoading, isError } = useGetMemberPointHistoryQuery({
+    enable: !!session.data?.accountId,
+    accountId: session.data?.accountId.toString() || undefined,
+    pageParam: {
+      page: currentPage,
+      size: 6,
+      sort: 'lastModifiedAt,DESC',
+    },
+  });
   return (
     <div className="flex w-full flex-col gap-y-14 px-40 pb-20 pt-18">
       <h2 className="text-h2 font-semibold text-sub-400">포인트 사용 내역</h2>
@@ -17,21 +33,58 @@ const PointsHistoryPage = () => {
           <UserTableHeaderModule>
             <UserTableHeaderAtom isFirst text="번호" width="200px" />
             <UserTableHeaderAtom text="사유" />
-            <UserTableHeaderAtom text="적립/사용" width="300px" />
+            <UserTableHeaderAtom text="일시" width="200px" />
+            <UserTableHeaderAtom text="적립/사용" width="250px" />
             <UserTableHeaderAtom isLast text="잔여 포인트" width="200px" />
           </UserTableHeaderModule>
           <tbody>
-            <EmptyContainer colSpan={4} />
+            {!data ? (
+              isLoading ? (
+                <EmptyContainer colSpan={4} text="로딩 중..." />
+              ) : isError ? (
+                <EmptyContainer colSpan={4} text="에러 발생" />
+              ) : (
+                <EmptyContainer colSpan={4} text="데이터 없음" />
+              )
+            ) : data.pointInfos.length < 1 ? (
+              <EmptyContainer colSpan={4} />
+            ) : (
+              data.pointInfos.map((item, idx) => (
+                <UserTableBodyModule key={item.getTime}>
+                  <UserTableBodyAtom isFirst>
+                    {(currentPage - 1) * 6 + idx + 1}
+                  </UserTableBodyAtom>
+                  <UserTableBodyAtom>{item.pointTitle}</UserTableBodyAtom>
+                  <UserTableBodyAtom>
+                    {dateConverter(item.getTime)}
+                  </UserTableBodyAtom>
+                  <UserTableBodyAtom>
+                    <div className="flex w-full items-center justify-center">
+                      <UserTextLabelAtom
+                        size="sm"
+                        text={`${item.usedPoint > 0 ? '+' : ''}${item.usedPoint.toLocaleString()}`}
+                        className={`${item.usedPoint > 0 ? 'bg-positive/10 text-positive' : 'bg-negative/10 text-negative'}`}
+                      />
+                    </div>
+                  </UserTableBodyAtom>
+                  <UserTableBodyAtom isLast>
+                    {item.totalPoint}
+                  </UserTableBodyAtom>
+                </UserTableBodyModule>
+              ))
+            )}
           </tbody>
         </UserTableContainer>
-        <div className="flex w-full items-center justify-center">
-          <PaginationModule
-            totalPages={10}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-            user
-          />
-        </div>
+        {data && data.pageInfo.totalElements > 0 && (
+          <div className="flex w-full items-center justify-center">
+            <PaginationModule
+              totalPages={data.pageInfo.totalPages}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+              user
+            />
+          </div>
+        )}
       </div>
     </div>
   );
