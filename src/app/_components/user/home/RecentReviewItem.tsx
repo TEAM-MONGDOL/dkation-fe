825360f76530904edd792df3a969472ea5c661f0
Ginieee <kaj1226@naver.com>
2024-08-10 22:53:52 +0900

    fix: RecentReviewItem에 ReviewInfoForMemberType 반영

--- src/app/_components/user/home/RecentReviewItem.tsx
+++ src/app/_components/user/home/RecentReviewItem.tsx
@@ -1,15 +1,15 @@
 import { StarRateEmptyIcon, StarRateIcon } from '@/_assets/icons';
-import { ReviewType } from '@/_types/adminType';
 import { dateConverter } from '@/_types/converter';
+import { ReviewInfoForMemberType } from '@/_types/userType';
 import Image from 'next/image';
 
 interface RecentReviewItemProps {
-  review: ReviewType;
+  review: ReviewInfoForMemberType;
 }
 
 const RecentReviewItem = ({ review }: RecentReviewItemProps) => {
   return (
-    <div className="flex h-[307px] w-[331px] shrink-0 flex-col rounded-xl border border-sub-100 text-sub-400">
+    <div className="flex h-[307px] w-[331px] shrink-0 snap-start flex-col rounded-xl border border-sub-100 text-sub-400">
       <div className="border-b border-sub-100 px-7 py-6">
         <div className="flex w-full items-center justify-between">
           <p className="text-4 font-medium text-sub-200">{review.wktPlace}</p>
@@ -35,7 +35,7 @@ const RecentReviewItem = ({ review }: RecentReviewItemProps) => {
         <h3 className="mt-4 text-h3 font-semibold">{review.reviewer}</h3>
       </div>
       <div className="flex w-full grow flex-col justify-between px-7 pb-7 pt-5">
-        <p className="font-medium">{review.blindedType}</p>
+        <p className="font-medium">{review.content}</p>
         <p className="w-full text-end text-4 text-sub-300">
           {dateConverter(review.lastModifiedAt)}
         </p>
