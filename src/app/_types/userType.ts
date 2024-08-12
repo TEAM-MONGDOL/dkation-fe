e4d9cf98b4d2107ebe451f0116a9d7b2af73d460
Ginieee <kaj1226@naver.com>
2024-08-12 11:33:49 +0900

    feat: wkDetailForMemberSchema 생성

--- src/app/_types/userType.ts
+++ src/app/_types/userType.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { pageInfoSchema } from './adminType';
+import { fileInfoSchema, pageInfoSchema } from './adminType';
 
 export const reviewInfoForMemberSchema = z.object({
   reviewId: z.number(),
@@ -16,6 +16,24 @@ export const reviewInfoForMemberSchema = z.object({
 export const reviewInfosForMemberSchema = z.object({
   reviewInfosForMember: z.array(reviewInfoForMemberSchema),
   pageInfo: pageInfoSchema,
+});
+
+export const wkDetailForMemberSchema = z.object({
+  title: z.string(),
+  totalRecruit: z.number(),
+  startDate: z.string(),
+  endDate: z.string(),
+  applyStartDate: z.string(),
+  applyEndDate: z.string(),
+  description: z.string(),
+  wktPlaceId: z.number(),
+  place: z.string(),
+  address: z.string(),
+  maxPeople: z.number(),
+  latitude: z.string(),
+  longitude: z.string(),
+  isApplied: z.boolean(),
+  files: z.array(fileInfoSchema).nullable().optional(),
 });
 
 // Type
