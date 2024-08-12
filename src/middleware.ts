da0fe4b55fb949741770895282de1cb5b705c2d6
Ginieee <kaj1226@naver.com>
2024-08-12 17:01:00 +0900

    fix: middleware 수정 (진행 중)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,44 @@
-// middleware.ts
-import { getToken } from 'next-auth/jwt';
 import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withAuth(
-  async function middleware(req) {
-    const { pathname } = req.nextUrl;
-
-    if (pathname.startsWith('/admin') && pathname !== '/admin/login') {
-      if (!req.nextauth.token || !req.nextauth.token.isAdmin) {
-        return NextResponse.redirect(new URL('/admin/login', req.url));
-      }
-    } else if (pathname !== '/login' && pathname !== '/') {
-      if (!req.nextauth.token) {
-        return NextResponse.redirect(new URL('/login', req.url));
-      }
-    }
-
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      // 로그인 상태일 경우 true 반환, 비로그인 시 false 반환
+      return !!token;
     },
   },
-);
+  pages: {
+    signIn: '/login', // 비로그인 시 리다이렉션될 로그인 페이지
+  },
+});
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const token =
+    req.cookies.get('next-auth.session-token') ||
+    req.cookies.get('__Secure-next-auth.session-token');
+  console.log('middleware', token);
+  // 로그인하지 않은 상태에서 보호된 페이지에 접근 시 로그인 페이지로 리다이렉트
+  if (!token && pathname !== '/' && pathname !== '/login') {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  // 로그인 상태에서 /login 페이지로 접근 시 홈으로 리다이렉션
+  if (pathname === '/login' && token) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // 다른 모든 요청은 기본적으로 통과
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ['/admin/:path*', '/points/:path*', '/workation/:path*'],
+  matcher: [
+    '/admin/:path*',
+    '/points/:path*',
+    '/workation/:path*',
+    '/support/:path*',
+    '/mypage/:path*',
+  ],
 };
