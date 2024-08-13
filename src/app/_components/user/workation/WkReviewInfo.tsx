3c965773ab82ba6e5ed756d820d6b13eddd18152
Ginieee <kaj1226@naver.com>
2024-08-13 15:34:27 +0900

    fix : WkReviewInfo 에서 ReviewInfo key 수정

--- src/app/_components/user/workation/WkReviewInfo.tsx
+++ src/app/_components/user/workation/WkReviewInfo.tsx
@@ -65,7 +65,7 @@ const WkReviewInfo = ({
                 <Image src={StarRateIcon} alt="StarRateIcon" />
               </div>
               <p>
-                {rating}점({ReviewInfo[rating.toString()]})
+                {rating}점({ReviewInfo[rating as keyof typeof ReviewInfo]})
               </p>
             </div>
           </div>
