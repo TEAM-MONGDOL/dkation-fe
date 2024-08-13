2b6cbb8ba8bae5e6e77e74f94d5469a3678d76de
gahyeon <gus_05@naver.com>
2024-08-13 17:50:37 +0900

    fix: UserTextLabelAtom에 fix size type 추가

--- src/app/_components/user/common/atoms/UserTextLabelAtom.tsx
+++ src/app/_components/user/common/atoms/UserTextLabelAtom.tsx
@@ -1,7 +1,7 @@
 interface UserTextLabelAtomProps {
   text: string;
   className?: string;
-  size: 'sm' | 'md' | 'lg';
+  size: 'sm' | 'md' | 'lg' | 'fix';
 }
 
 const UserTextLabelAtom = ({
@@ -13,6 +13,7 @@ const UserTextLabelAtom = ({
     sm: 'px-3.5 py-1',
     md: 'px-5 py-2',
     lg: 'px-5 py-2.5',
+    fix: 'w-24 py-1',
   };
 
   return (
