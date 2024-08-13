9fc5d59a8c38e588d4bfa21525bb78485d3cb2cc
Ginieee <kaj1226@naver.com>
2024-08-13 13:27:45 +0900

    fix: 유저 내브바 로그인/로그아웃 플로우 구현

--- src/app/_components/user/common/atoms/UserNavButtonAtom.tsx
+++ src/app/_components/user/common/atoms/UserNavButtonAtom.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import UserButtonAtom from '@/_components/user/common/atoms/UserButtonAtom';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { useGetMemberDetailQuery } from '@/_hooks/common/useGetMemberDetailQuery';
 
 const UserNavButtonAtom = () => {
@@ -10,32 +10,52 @@ const UserNavButtonAtom = () => {
   const session = useSession();
   const accountId = String(session.data?.accountId);
 
-  const handleLogout = () => {
-    // 세션스토리지에서 토큰 삭제코드 추가
-    router.push('/login');
+  const handleLogout = async () => {
+    const result = await signOut({
+      redirect: false,
+      callbackUrl: '/',
+    });
+
+    if (result.url) {
+      router.push(result.url);
+    }
   };
+  const { data: memberDetail, isError: memberDetailIsError } =
+    useGetMemberDetailQuery({ accountId });
 
-  const { data: memberDetail } = useGetMemberDetailQuery({ accountId });
-
-  return (
-    <div className="ml-auto flex gap-3 text-4">
-      <UserButtonAtom
-        className="font-semibold"
-        buttonStyle="yellow"
-        text={`${memberDetail?.pointQuantity} Point`}
-        type="button"
-        size="header"
-        onClick={() => router.push('/id/point')} // 라우팅 주소 변경 예정
-      />
-      <UserButtonAtom
-        text="로그아웃"
-        size="header"
-        buttonStyle="white"
-        type="button"
-        onClick={handleLogout}
-      />
-    </div>
-  );
+  if (!session.data) {
+    return (
+      <div className="ml-auto flex gap-3 text-4">
+        <UserButtonAtom
+          text="로그인"
+          size="header"
+          buttonStyle="white"
+          type="button"
+          onClick={() => router.push('/login')}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="ml-auto flex gap-3 text-4">
+        <UserButtonAtom
+          className="font-semibold"
+          buttonStyle="yellow"
+          text={`${memberDetail?.pointQuantity || 0} Point`}
+          type="button"
+          size="header"
+          onClick={() => router.push('/id/point')} // 라우팅 주소 변경 예정
+        />
+        <UserButtonAtom
+          text="로그아웃"
+          size="header"
+          buttonStyle="white"
+          type="button"
+          onClick={handleLogout}
+        />
+      </div>
+    );
+  }
 };
 
 export default UserNavButtonAtom;
