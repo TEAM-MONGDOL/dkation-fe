f7d043b6136037b8e8683862d52127f2f94952a0
Ginieee <kaj1226@naver.com>
2024-08-16 00:02:23 +0900

    fix: 내브바 구조 수정

--- src/app/_components/user/common/containers/UserNavBarContainer.tsx
+++ src/app/_components/user/common/containers/UserNavBarContainer.tsx
@@ -17,30 +17,30 @@ const UserNavBarContainer = () => {
     { href: '/mypage', text: '마이페이지' },
   ];
   return (
-    <div className="mx-28 flex h-20 items-center">
-      <div className="mr-28 flex">
-        <Image src={logo} alt="logo" className="w-[145px]" />
+    <div className="flex h-20 w-full items-center justify-between gap-x-5 px-28">
+      <div className="flex min-w-[100px] max-w-[145px] items-center justify-center">
+        <Image src={logo} alt="logo" className="w-full" />
       </div>
-      <div className="flex gap-10">
+      <div className="flex lg:gap-5 xl:gap-10">
         {menuItems.map((item) => (
           <div
             role="presentation"
             key={item.href}
-            className="flex h-20 w-[90px] cursor-pointer flex-col items-center justify-center gap-2"
+            className="flex h-20 cursor-pointer flex-col items-center justify-center gap-2"
           >
             <Link href={item.href}>
               <p
                 className={
                   (item.href === '/' && currentPath === '/') ||
                   (item.href !== '/' && currentPath.startsWith(item.href))
-                    ? 'text-center font-semibold'
-                    : 'text-center'
+                    ? 'px-5 text-center font-semibold'
+                    : 'px-5 text-center'
                 }
               >
                 {item.text}
               </p>
               <div
-                className={`h-[3px] w-[100px] translate-y-2 ${(item.href === '/' && currentPath === '/') || (item.href !== '/' && currentPath.startsWith(item.href)) ? 'bg-primary' : 'bg-transparent'}`}
+                className={`h-[3px] w-full translate-y-2 ${(item.href === '/' && currentPath === '/') || (item.href !== '/' && currentPath.startsWith(item.href)) ? 'bg-primary' : 'bg-transparent'}`}
               />
             </Link>
           </div>
