bbae5bd9fe51e44a7402672ce2deae5ad8687c41
Ginieee <kaj1226@naver.com>
2024-08-16 01:13:45 +0900

    fix: 테이블 body, header atom에 textAlign 속성 추가

--- src/app/_components/user/common/atoms/UserTableHeaderAtom.tsx
+++ src/app/_components/user/common/atoms/UserTableHeaderAtom.tsx
@@ -3,6 +3,7 @@ interface UserTableHeaderAtomProps {
   width?: string;
   isFirst?: boolean;
   isLast?: boolean;
+  textAlign?: 'left' | 'center' | 'right';
 }
 
 const UserTableHeaderAtom = ({
@@ -10,10 +11,11 @@ const UserTableHeaderAtom = ({
   width,
   isFirst,
   isLast,
+  textAlign = 'center',
 }: UserTableHeaderAtomProps) => {
   return (
     <th
-      className={`px-5 py-2.5 text-center align-middle text-4 font-normal text-sub-300 ${isFirst ? 'rounded-l pl-6 xl:pl-10' : ''} ${isLast ? 'rounded-r pr-6 xl:pr-10' : ''}`}
+      className={`px-5 py-2.5 ${textAlign === 'center' ? 'text-center' : textAlign === 'left' ? 'text-start' : 'text-end'} align-middle text-4 font-normal text-sub-300 ${isFirst ? 'rounded-l pl-6 xl:pl-10' : ''} ${isLast ? 'rounded-r pr-6 xl:pr-10' : ''}`}
       style={{ width }}
     >
       {text}
