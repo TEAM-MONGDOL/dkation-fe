999beb2058f3bf7a196cd4b9a78892fba839a5e2
Ginieee <kaj1226@naver.com>
2024-08-17 16:35:26 +0900

    fix: useGetPointSupplyDetailQuery에 페이지네이션 적용

--- src/app/_hooks/admin/useGetPointSupplyDetailQuery.ts
+++ src/app/_hooks/admin/useGetPointSupplyDetailQuery.ts
@@ -2,17 +2,28 @@ import { useQuery } from '@tanstack/react-query';
 import { pointSupplyDetailSchema } from '@/_types/adminType';
 import api from '../Axios';
 
-export const useGetPointSupplyDetailQueryKey = 'useGetPointSupplyDetailQuery';
+export const getPointSupplyDetailQueryKey = (id: string) =>
+  `useGetPointSupplyDetailQuery-${id}`;
 
 export const useGetPointSupplyDetailQuery = ({
   supplyId,
+  pageable: { page, size },
 }: {
   supplyId: number;
+  pageable: {
+    page: number;
+    size: number;
+  };
 }) => {
   return useQuery({
-    queryKey: [useGetPointSupplyDetailQueryKey],
+    queryKey: [getPointSupplyDetailQueryKey(supplyId.toString()), page, size],
     queryFn: async () => {
-      const res = await api.get(`/api/point/supply/${supplyId}`);
+      const res = await api.get(`/api/point/supply/${supplyId}`, {
+        params: {
+          page,
+          size,
+        },
+      });
       return pointSupplyDetailSchema.parse(res.data.data);
     },
   });
