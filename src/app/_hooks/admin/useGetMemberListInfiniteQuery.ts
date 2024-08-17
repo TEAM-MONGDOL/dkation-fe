b831cc430437d6429ebac3cb6374f64ef475a537
Ginieee <kaj1226@naver.com>
2024-08-17 17:31:50 +0900

    fix: 단체 포인트 지급 대상 조회 이름 검색 적용

--- src/app/_hooks/admin/useGetMemberListInfiniteQuery.ts
+++ src/app/_hooks/admin/useGetMemberListInfiniteQuery.ts
@@ -5,26 +5,38 @@ import api from '../Axios';
 export const useGetMemberListInfiniteQueryKey = 'useGetMemberListInfiniteQuery';
 
 export const useGetMemberListInifiniteQuery = ({
+  searchText,
   department,
   pageable,
 }: {
+  searchText?: string;
   department?: string;
-  pageable: { page: number; size: number; sort?: string };
+  pageable: { page: number; size: number; sort: string };
 }) => {
   return useInfiniteQuery({
-    queryKey: [useGetMemberListInfiniteQueryKey],
+    queryKey: [
+      useGetMemberListInfiniteQueryKey,
+      searchText,
+      department,
+      pageable.sort,
+    ], // Include department and sort in the query key
     queryFn: async ({ pageParam = pageable }) => {
+      const { page, size, sort } = pageParam; // Destructure page, size, and sort from pageParam
       const res = await api.get('/api/member', {
-        params: { department, ...pageParam },
+        params: { name: searchText, department, page, size, sort }, // Ensure the sort parameter is passed here
       });
       return memberListSchema.parse(res.data.data);
     },
-    initialPageParam: { page: 1, size: 100 },
+    initialPageParam: { page: 1, size: 100, sort: 'name,ASC' }, // Include sort in the initialPageParam
     getNextPageParam: (lastPage) => {
       return lastPage.pageInfo.totalElements === 0 ||
         lastPage.pageInfo.totalPages - 1 === lastPage.pageInfo.pageNum
         ? undefined
-        : { page: lastPage.pageInfo.pageNum + 2, size: pageable.size };
+        : {
+            page: lastPage.pageInfo.pageNum + 2,
+            size: pageable.size,
+            sort: pageable.sort,
+          }; // Include sort in the nextPageParam
     },
   });
 };
